7b53d4833af61b331cf882ee7e63b36722d85a1a
mkersten <mkersten>
2005-11-01 23:16:42 +0000

    *** empty log message ***

--- sandbox/index.php
+++ sandbox/index.php
@@ -97,19 +97,21 @@
 			newsgroup archive</a>)</li>
 			</ul>
 		</div>
-	</div>
-	
-	<div id="rightcolumn">
-		<div class="sideitem">
-			<h6>News</h6>
-			<ul>
-				<li><a href="#">First public release</a><span class="dates"> (October 28, 2005)</span></li>
-				<li>Ed Burnette: <a href="http://www.eclipsezone.com/eclipse/forums/t20933.html">Mylar is the next killer app for Eclipse..</a><span class="dates"> (July 2005)</span></li>
-				<li><a href="#">User study preview release</a><span class="dates"> (July 11, 2005)</span></li>
-				<li><a href="#">Eclipse.org project created</a><span class="dates"> (June 13, 2005)</span></li>
-			</ul>
+		
+		<div id="rightcolumn">
+			<div class="sideitem">
+				<h6>News</h6>
+				<ul>
+					<li><a href="#">First public release</a><span class="dates"> (October 28, 2005)</span></li>
+					<li>Ed Burnette: <a href="http://www.eclipsezone.com/eclipse/forums/t20933.html">Mylar is the next killer app for Eclipse..</a><span class="dates"> (July 2005)</span></li>
+					<li><a href="#">User study preview release</a><span class="dates"> (July 11, 2005)</span></li>
+					<li><a href="#">Eclipse.org project created</a><span class="dates"> (June 13, 2005)</span></li>
+				</ul>
+			</div>
 		</div>
 	</div>
+	
+
 	<hr class="clearer" />
 </div>
 EOHTML;

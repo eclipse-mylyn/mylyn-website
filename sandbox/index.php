ebc394ca19dc92b80e9f050f8ccfe53f2017bef0
mkersten <mkersten>
2005-11-01 23:55:50 +0000

    *** empty log message ***

--- sandbox/index.php
+++ sandbox/index.php
@@ -71,37 +71,35 @@
 			<ul>
 				<li>
                     <a href="http://dev.eclipse.org/mailman/listinfo/mylar-announce">
-					mylar-announce@eclipse.org</a> release and event announcements
+					mylar-announce@eclipse.org: </a> release and event announcements
                     (<a href="http://dev.eclipse.org/mhonarc/lists/mylar-announce/maillist.html">archive</a>)</li>
 				<li>
                     <a href="http://dev.eclipse.org/mailman/listinfo/mylar-users">
-					mylar-users@eclipse.org</a>questions, discussions and suggestions
+					mylar-users@eclipse.org: </a>questions, discussions and suggestions
 					(<a href="http://dev.eclipse.org/mhonarc/lists/mylar-users/maillist.html">archive</a>)</li>
 				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=%5Bdiscussion%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
 					Bugzilla report discussions</a> are used to discuss ideas, enhancements, and UI issues.</li>
 			</ul>
 		</div>
-
-		<div id="rightcolumn">
-			<div class="sideitem">
-				<h6>Releases</h6>
-				<ul>
-					<li><a href="#">Mylar 0.4.0</a><span class="dates"> (October 28, 2005)</span><br>&nbsp;</li>
-					<li><a href="#">Mylar 0.3.12</a><span class="dates"> (October 28, 2005)</span><br>&nbsp;</li>
-				</ul>
-			</div>
-			<div class="sideitem">
-				<h6>News</h6>
-				<ul>
-					<li>Ed Burnette: <a href="http://www.eclipsezone.com/eclipse/forums/t20933.html">Mylar is the next killer app for Eclipse..</a><span class="dates"> (July 2005)</span><br>&nbsp;</li>
-					<li><a href="#">User study started</a><span class="dates"> (July 11, 2005)</span><br>&nbsp;</li>
-					<li><a href="#">Eclipse.org project created</a><span class="dates"> (June 13, 2005)</span><br>&nbsp;</li>
-				</ul>
-			</div>
-		</div>
-
 	</div>
-	
+
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>Releases</h6>
+			<ul>
+				<li><a href="#">Mylar 0.4.0</a><span class="dates"> (October 28, 2005)</span><br>&nbsp;</li>
+				<li><a href="#">Mylar 0.3.12</a><span class="dates"> (October 28, 2005)</span><br>&nbsp;</li>
+			</ul>
+		</div>
+		<div class="sideitem">
+			<h6>News</h6>
+			<ul>
+				<li>Ed Burnette: <a href="http://www.eclipsezone.com/eclipse/forums/t20933.html">Mylar is the next killer app for Eclipse..</a><span class="dates"> (July 2005)</span><br>&nbsp;</li>
+				<li><a href="#">User study started</a><span class="dates"> (July 11, 2005)</span><br>&nbsp;</li>
+				<li><a href="#">Eclipse.org project created</a><span class="dates"> (June 13, 2005)</span><br>&nbsp;</li>
+			</ul>
+		</div>
+	</div>	
 
 	<hr class="clearer" />
 </div>

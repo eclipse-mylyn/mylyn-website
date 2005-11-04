e30d7a8332cde2ebb87c9ffbdf24f509b6b314d8
mkersten <mkersten>
2005-11-04 17:01:19 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 				past efforts, and sharing expertise.</p>
 				<p><i>Please note that the Mylar site uses the still experimental
 				<a href="http://eclipse.org/phoenix/">Phoenix infrastructure.<a></i></p>
-				<p><b><font bgcolor="#CAC8E6">Announcement: the Eclipse 3.2M3 integration build releaased today
+				<p><b><font color="#CAC8E6">Announcement: the Eclipse 3.2M3 integration build releaased today
 				is not supported by 0.4.0, but will be supported by tonight's 0.4.1 
 				release.</b></font></p>
 		  	</td>

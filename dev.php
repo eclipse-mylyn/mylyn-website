370fceb0765249e5a20815628b672c030912fa3c
mkersten <mkersten>
2005-11-07 18:41:14 +0000

    *** empty log message ***

--- dev.php
+++ dev.php
@@ -33,14 +33,31 @@
 	<div id="all">
 		<div align="center"><h1>$pageTitle</h1></div>
 
-        Mylar is evolving 
+        <p>Mylar is evolving 
 		rapidly, and slowly stabilizing initial APIs and extension points. If you plan on extending 
 		the tool we suggest that you bring this up as a discussion on
-		<a href="mailto:mylar-dev@eclipse.org">mylar-dev@eclipse.org</a>.
-		Development is done in an XP style using Mylar, with weekly planning messages 
+		<a href="mailto:mylar-dev@eclipse.org">mylar-dev@eclipse.org</a>.</p>
+		<p>Development is done in an XP style using Mylar, with weekly planning messages 
 		sent to the developer mailing list. Every task that results in code or resource 
-		modification should correspond to a bugzilla report to show up in the plan.
+		modification should correspond to a bugzilla report to show up in the plan.  The weekly plan 
+		is sent out each Monday, if you would like a task included mark the report P2 or higher.</p>
           
+		<div class="homeitem">
+			<h3>Conventions</h3>
+			<ul>
+				<li><b>Bugzilla priorities:</b> P1: do this week, P2: should do 
+				this week, P3: considered for next week, P4/5: requires user input</li>
+				<li><b>Code:</b> conventions are all checked into each project's .settings folder.
+				Default formatter is modfied to wrap at 160 columns. Coode conventions are documented
+				at <a href="http://java.sun.com/docs/codeconv/">http://java.sun.com/docs/codeconv</a></li>
+				<li><b>Patches</b>: right-click the corresponding project and select Team -&gt; Create 
+				Patch.&nbsp; Name according to project (e.g. patch-core.txt, patch-core.tests.txt).
+				</li>
+				<li><b>Committing:</b> enure no warnings, and that org.eclipse.mylar.tests.AllTests 
+				passes with no console output.</li> 
+			</ul>
+		</div>     
+
 		<div class="homeitem">
 			<h3>Developer Community</h3>
 			<ul>
@@ -48,28 +65,12 @@
                     <a href="http://dev.eclipse.org/mailman/listinfo/mylar-dev">
 					mylar-dev@eclipse.org:</a> developer discussion
                     [<a href="http://dev.eclipse.org/mhonarc/lists/mylar-dev/maillist.html">archive</a>]</li>
-				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=helpwanted&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-					Bugzilla reports marked helpwanted</a> are a starting point for those interested in contributing</li>
 				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=%5Bdiscussion%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
 					Bugzilla discussions</a> are used for ideas, enhancements, and UI issues.</li>
+				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=helpwanted&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
+					Bugzilla reports marked helpwanted</a> are a starting point for those interested in contributing</li>
 			</ul>
-		</div>        
-
-		<div class="homeitem">
-			<h3>Conventions</h3>
-			<ul>
-				<li><b>Planning:</b> the weekly plan is sent out each Monday, if you would 
-				like a task included mark the report P2 or higher.</li>
-				<li><b>Bugzilla priorities:</b> P1: do this week, P2: should do 
-				this week, P3: considered for next week, P4/5: requires user input</li>
-				<li><b>Patches</b>: right-click the 
-				corresponding project and select Team -&gt; Create 
-				Patch.&nbsp; Name according to project (e.g. patch-core.txt, patch-core.tests.txt).
-				</li>
-				<li><b>Committing:</b> enure no warnings, and that org.eclipse.mylar.tests.AllTests 
-				passes with no console output.</li>
-			</ul>
-		</div>     
+		</div> 
           
 		<h4>Workspace Setup</h4>
         - Building Mylar feature from HEAD requires Eclipse 

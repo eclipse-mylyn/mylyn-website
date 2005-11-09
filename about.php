81a66be62fc912994bc73398b61b7ac7f020e295
mkersten <mkersten>
2005-11-09 02:48:54 +0000

    *** empty log message ***

--- about.php
+++ about.php
@@ -67,8 +67,8 @@
 		<div class="sideitem">
 			<h6>&quot;Mylar&quot; is</h6>
 			<ul>
-				<li>An aluminized film used to avoid blindness when staring at a solar eclipse<br>&nbsp;</li>
-				<li>A focused user interface used to avoid information blindness when staring at Eclipse</li>
+				<li>An aluminized film used to avoid blindness when staring at a solar eclipse.<br>&nbsp;</li>
+				<li>A focused user interface used to avoid information blindness when staring at Eclipse.</li>
 			</ul> 
 		</div> 
 	</div>

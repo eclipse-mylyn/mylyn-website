b35439efe9ef6f8013b3d4538045451db347ca84
mkersten <mkersten>
2005-12-02 03:02:24 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 					<a href="http://dev.eclipse.org/newslists/news.eclipse.technology.mylar/maillist.html">[archive]</a></li>
 				<li>
                     <a href="http://dev.eclipse.org/mailman/listinfo/mylar-announce">
-					mylar-announce@eclipse.org:</a> major releases and events 
+					mylar-announce@eclipse.org:</a> major release and event announcements
 					[<a href="http://dev.eclipse.org/mhonarc/lists/mylar-dev/maillist.html">archive</a>]</li>
 				<li>
                     <a href="http://dev.eclipse.org/mailman/listinfo/mylar-dev">

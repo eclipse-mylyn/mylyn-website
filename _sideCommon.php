ae1ac0c65aacbd86a270b4970f825f5814832127
mkersten <mkersten>
2005-12-02 21:56:45 +0000

    Progress on: Bug 117599: improve web site and phoneix use https://bugs.eclipse.org/bugs/show_bug.cgi?id=117599

--- _sideCommon.php
+++ _sideCommon.php
@@ -1,7 +1,7 @@
 <?php
     $commonside = <<<EOHTML
 	<div class="sideitem">
-		<h6>Latest Release</h6>
+		<h6>Latest Release..</h6>
 		<ul>
 			<li><a href="dl.php">Mylar 0.4.3</a><span class="dates"> 
 			<br>(Nov. 21, 2005)</span><br>&nbsp;</li>
--- index.php
+++ index.php
@@ -20,6 +20,8 @@
 	#
 		
 	# Paste your HTML content between the EOHTML markers!	
+	include("_sideCommon.php");
+	
 	$html = <<<EOHTML
   
 <div id="maincontent">
@@ -79,15 +81,7 @@
 	</div> 
 
 	<div id="rightcolumn">
-		<div class="sideitem">
-			<h6>Latest Release</h6>
-			<ul>
-				<li><a href="dl.php">Mylar 0.4.3</a><span class="dates"> 
-				<br>(Nov. 21, 2005)</span><br>&nbsp;</li>
-				<li>Mylar 0.4.4</a><span class="dates"> 
-				<br>(planned for Dec. 2)</span><br>&nbsp;</li>
-			</ul> 
-		</div> 
+		$commonside
 		<div class="sideitem">
 			<h6>News & Blogs</h6>
 			<ul>

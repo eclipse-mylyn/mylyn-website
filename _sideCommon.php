208f594631c75cc49023999c9095875cec821b04
mkersten <mkersten>
2005-12-02 21:54:38 +0000

    Progress on: Bug 117599: improve web site and phoneix use https://bugs.eclipse.org/bugs/show_bug.cgi?id=117599

---
+++ _sideCommon.php
@@ -0,0 +1,14 @@
+<?php
+    $commonside = <<<EOHTML
+	<div class="sideitem">
+		<h6>Latest Release</h6>
+		<ul>
+			<li><a href="dl.php">Mylar 0.4.3</a><span class="dates"> 
+			<br>(Nov. 21, 2005)</span><br>&nbsp;</li>
+			<li>Mylar 0.4.4</a><span class="dates"> 
+			<br>(planned for Dec. 2)</span><br>&nbsp;</li>
+		</ul> 
+	</div> 
+EOHTML
+
+?>

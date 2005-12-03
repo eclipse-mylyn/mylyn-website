a854bba1e00ded6702fd70424d5c749bdfd6e24e
mkersten <mkersten>
2005-12-03 04:08:52 +0000

    Progress on: long: web site update

--- dev.php
+++ dev.php
@@ -107,13 +107,22 @@
 				<li>
                     <a href="http://dev.eclipse.org/mailman/listinfo/mylar-dev">
 					mylar-dev@eclipse.org:</a> developer discussions 
-					<a href="http://dev.eclipse.org/mhonarc/lists/mylar-dev/maillist.html">[archive]</a></li>
+					<a href="http://dev.eclipse.org/mhonarc/lists/mylar-dev/maillist.html">[archive]</a>
+					<br>&nbsp;
+				</li>
+				
 				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=%5Bdiscussion%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-					Bugzilla discussions</a> are used for ideas, enhancements, and UI issues.</li>
+					Bugzilla discussions</a> are used for ideas, enhancements, and UI issues.
+					<br>&nbsp;
+				</li>
 				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=helpwanted&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-					Bugzilla reports marked helpwanted</a> are a starting point for those interested in contributing</li>
+					Bugzilla reports marked helpwanted</a> are a starting point for those interested in contributing
+					<br>&nbsp;
+				</li>
 				<li><a href="http://www.eclipse.org/eclipse/development/main.html">The Eclipse development page</a> documents
-					conventions and resources.</li>
+					conventions and resources.
+					<br>&nbsp;
+				</li>
 				<li><a href="http://dev.eclipse.org/viewcvs/indextech.cgi/org.eclipse.mylar/">CVS repository</a> web interface</li>
 			</ul>
 		</div>

6627e27a8303fc4e5d81c3dc5fbc0981d8ab7d4e
mkersten <mkersten>
2005-12-12 22:53:34 +0000

    Progress on: long: web site update

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,16 +1,15 @@
 <?php
-
 	# Set the theme for your project's web pages.
 	# See the Committer Tools "How Do I" for list of themes
 	# https://dev.eclipse.org/committers/ 
 	$theme = "Phoenix";
 
-	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	$Nav->addNavSeparator("Mylar Technology Project", 	"index.php");
-	$Nav->addCustomNav("About", 		"/mylar/about.php", 	"_self", 1);
-	$Nav->addCustomNav("Downloads", 	"/mylar/dl.php",	"_self", 1);
-	$Nav->addCustomNav("Developers", 	"dev.php", 	"_self", 1);
-	$Nav->addNavSeparator("Documentation", "doc.php", 	"_self", 1);
-	$Nav->addCustomNav("New & Noteworthy", "doc/new.php", 		"_self", 1);
+	$Nav->addNavSeparator("Mylar Technology Project", 	"/mylar/index.php");
+	$Nav->addCustomNav("About", 			"/mylar/about.php", 	"_self", 1);
+	$Nav->addCustomNav("Downloads", 		"/mylar/dl.php",		"_self", 1);
+	$Nav->addCustomNav("Developers", 		"/mylar/dev.php", 		"_self", 1);
+	$Nav->addNavSeparator("Documentation", 	"/mylar/doc.php", 		"_self", 1);
+	$Nav->addCustomNav("New & Noteworthy", 	"/mylar/doc/new.php", 	"_self", 1);
+	$Nav->addCustomNav("FAQ", 				"/mylar/doc/faq.php", 	"_self", 1);
 ?>

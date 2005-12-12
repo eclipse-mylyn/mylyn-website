cd99ef1a5e63ec324285a74e97b49f55dfaa2b72
mkersten <mkersten>
2005-12-12 22:46:22 +0000

    Progress on: long: web site update

--- _projectCommon.php
+++ _projectCommon.php
@@ -8,8 +8,8 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	$Nav->addNavSeparator("Mylar Technology Project", 	"index.php");
-	$Nav->addCustomNav("About", 		"about.php", 	"_self", 1);
-	$Nav->addCustomNav("Downloads", 	"dl.php",	"_self", 1);
+	$Nav->addCustomNav("About", 		"/mylar/about.php", 	"_self", 1);
+	$Nav->addCustomNav("Downloads", 	"/mylar/dl.php",	"_self", 1);
 	$Nav->addCustomNav("Developers", 	"dev.php", 	"_self", 1);
 	$Nav->addNavSeparator("Documentation", "doc.php", 	"_self", 1);
 	$Nav->addCustomNav("New & Noteworthy", "doc/new.php", 		"_self", 1);
--- doc/new.php
+++ doc/new.php
@@ -9,7 +9,7 @@
 	$pageKeywords	= "Mylar, Eclipse";
 	$pageAuthor		= "Mik Kersten";
 	
-	$filename = "http://www.eclipse.org/mylar/doc/new.html" ;
+	$filename = "http://www.eclipse.org/mylar/doc/new.html";
     $dataFile = fopen( $filename, "r" ) ;
     if ( $dataFile )  {
     	while (!feof($dataFile))   {
@@ -19,12 +19,26 @@
     } else  {
     	die( "fopen failed for $filename" ) ;
     }
-	
-	# End: page-specific settings
-		
+
 	# Paste your HTML content between the EOHTML markers!	
 	
+	include("_sideCommon.php");
+		
+	$html = <<<EOHTML
 
+<div id="maincontent">
+	<div id="midcolumn">
+		<div align="center"><h1>$pageTitle</h1></div>
+        $contents
+	</div>
+	
+	<div id="rightcolumn">
+		$commonside
+	</div>
+</div>
+
+
+EOHTML;
 
 
 	# Generate the web page

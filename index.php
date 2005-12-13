3009d472e21045718f2c27de10e5408d3a803713
mkersten <mkersten>
2005-12-13 22:04:52 +0000

    Progress on: update to new project web site format http://www.eclipse.org/projects/dev_process/project-status-infrastructure.php

--- index.php
+++ index.php
@@ -100,7 +100,7 @@
 			</ul>
 		</div>
 		<div class="sideitem">
-			<h6>Status</h6>
+			<h6>Project Status</h6>
 			<ul>
 				<li><a href="http://eclipse.org/projects/dev_process/validation-phase.php">Incubation Phase</a>
 				</li>

c0faa14b4fc2877678136518acac232a3803d553
mkersten <mkersten>
2005-12-19 01:02:55 +0000

    Progress on: long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -5,8 +5,8 @@
 		<ul>
 			<li><a href="dl.php">Mylar 0.4.5</a><span class="dates"> 
 			<br>(Dec. 16, 2005)</span><br>&nbsp;</li>
-			<li><i>Mylar 0.4.7</i></a><span class="dates"> 
-			<br>(after Christmas)</span><br>&nbsp;</li>
+			<li><i>Mylar 0.4.7</a><span class="dates"> 
+			<br>(after Christmas)</span></i><br>&nbsp;</li>
 		</ul> 
 	</div> 
 EOHTML

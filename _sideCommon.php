6cc0d6cf49d03113a8201207b25bc408d9515f3b
mkersten <mkersten>
2006-01-14 03:07:55 +0000

    Progress on: long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -3,10 +3,10 @@
 	<div class="sideitem">
 		<h6>Latest Release</h6>
 		<ul>
-			<li><a href="dl.php">Mylar 0.4.6</a><span class="dates"> 
-			<br>(Dec. 16, 2005)</span><br>&nbsp;</li>
-			<li><i>Mylar 0.4.7</a><span class="dates"> 
-			<br>(postponed to:<br> Jan. 13, 2006)</span></i><br>&nbsp;</li>
+			<li><a href="dl.php">Mylar 0.4.7</a><span class="dates"> 
+			<br>(Jan. 13, 2005)</span><br>&nbsp;</li>
+			<li><i>Mylar 0.4.8</a><span class="dates"> 
+			<br>(planned for:<br> Jan. 27, 2006)</span></i><br>&nbsp;</li>
 		</ul> 
 	</div> 
 EOHTML

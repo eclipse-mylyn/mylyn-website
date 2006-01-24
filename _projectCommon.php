328428d8e13774ee666e196919a0fe051f38e4bd
mkersten <mkersten>
2006-01-24 01:01:59 +0000

    Progress on: long: web site update

--- _projectCommon.php
+++ _projectCommon.php
@@ -5,10 +5,10 @@
 	$theme = "Phoenix";
 
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	$Nav->addNavSeparator("Mylar Project", 	"/mylar/index.php");
+	$Nav->addNavSeparator("Mylar", 	"/mylar/index.php");
 	$Nav->addCustomNav("About", 			"/mylar/about.php", 	"_self", 1);
-	$Nav->addCustomNav("Downloads", 		"/mylar/dl.php",		"_self", 1);
 	$Nav->addCustomNav("Bugs", 				"/mylar/bugs.php",		"_self", 1);
+	$Nav->addCustomNav("Downloads", 		"/mylar/dl.php",		"_self", 1);
 	$Nav->addNavSeparator("Documentation", 	"/mylar/doc.php", 		"_self", 1);
 	$Nav->addCustomNav("New & Noteworthy", 	"/mylar/doc/new.php", 	"_self", 1);
 	$Nav->addCustomNav("FAQ", 				"/mylar/doc/faq.php", 	"_self", 1);
--- bugs.php
+++ bugs.php
@@ -25,9 +25,11 @@
 <div id="maincontent">
 	<div id="midcolumn">
 		<div align="center"><h1>$pageTitle</h1></div>
-		
+		<p>Partly thanks to the Bugzilla integration that it provides, the
+		Mylar project makes heavy use of Bugzilla for tracking bugs, planning,
+		and discussing design issues.</p> 
 		<p>Please <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Mylar">
-		submit bug reports</a> for any problems and enhancement requests.</p>
+		submit bug reports</a> for any problems and enhancement requests</p>
 		
 		<div class="homeitem3col">
 			<h3>Bugzilla Queries</h3>
@@ -35,8 +37,11 @@
 				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&priority=P1&priority=P2&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
 				Planned for next release</a>
 				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&priority=P1&priority=P2&priority=P3&priority=P4&priority=P5&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-				All open reports</a>
-				<li></li>
+				All open reports</a> 
+				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&op_sys=Linux&op_sys=Linux-GTK&op_sys=Linux-Motif&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
+				Linux-specific reports</a> 
+				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&op_sys=Mac+OS&op_sys=MacOS+X&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
+				MacOS-specific reports</a> 
 			</ul>
 		</div>
 		<p>&nbsp;</p>

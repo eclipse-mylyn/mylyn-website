da1a223db20865400ad369156deab3d04cc44f0e
mkersten <mkersten>
2006-01-25 16:51:01 +0000

    Progress on: long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -6,7 +6,7 @@
 			<li><a href="dl.php">Mylar 0.4.7</a><span class="dates"> 
 			<br>(Jan. 13, 2005)</span><br>&nbsp;</li>
 			<li><i>Mylar 0.4.8</a><span class="dates"> 
-			<br>(planned for:<br> Jan. 27, 2006)</span></i><br>&nbsp;</li>
+			<br>(planned for:<br> Jan. 27, 2006)</span></i></li>
 		</ul> 
 	</div> 
 EOHTML

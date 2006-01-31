480e5d1a287bdfc3379258af027b4e680f98d89b
mkersten <mkersten>
2006-01-31 19:19:51 +0000

    Progress on: long: web site update

--- dev.php
+++ dev.php
@@ -1,42 +1,28 @@
 <?php
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/bug.class.php");
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php");	
+        require_once($_SERVER['DOCUMENT_ROOT'] .
+"/eclipse.org-common/system/app.class.php");   
+        require_once($_SERVER['DOCUMENT_ROOT']
+."/eclipse.org-common/system/nav.class.php");   
+        require_once($_SERVER['DOCUMENT_ROOT']
+."/eclipse.org-common/system/menu.class.php");  
+        require_once($_SERVER['DOCUMENT_ROOT']
+."/projects/common/bug.class.php");
+        require_once($_SERVER['DOCUMENT_ROOT']
+."/projects/common/project-info.class.php");    
 
-	$App 	= new App();	
-	$Nav	= new Nav();	
-	$Menu 	= new Menu();		
-	
-    include($App->getProjectCommon());   
-     
-	$projectInfo = new ProjectInfo("technology.mylar");     
-	
-	# All on the same line to unclutter the user's desktop'
+        $App    = new App();    $Nav    = new Nav();    $Menu   = new Menu();   
+        ob_start();
 
-	#*****************************************************************************
-	#
-	#****************************************************************************
-	
-	#
-	# Begin: page-specific settings.  Change these. 
+        include($App->getProjectCommon());   
+		$projectInfo = new ProjectInfo("technology.mylar");
+
+  
 	$pageTitle 		= "Mylar Developers";
 	$pageKeywords	= "Mylar, Eclipse";
 	$pageAuthor		= "Mik Kersten";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
-
-	# End: page-specific settings
-	#
 		
-	# Paste your HTML content between the EOHTML markers!	
 	include("_sideCommon.php");
-	$html = <<<EOHTML
+?>   
 
 <div id="maincontent">
 	<div id="midcolumn">
@@ -157,8 +143,6 @@
         </div>
 	</div>
 </div>
-
-EOHTML;
 
 <?php
     $html = ob_get_contents();

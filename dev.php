c3740035a2ee6b9951d7dcf0de309bd3583e3acd
mkersten <mkersten>
2006-01-31 16:56:11 +0000

    Progress on: long: web site update

--- dev.php
+++ dev.php
@@ -121,6 +121,16 @@
 	
 	<div id="rightcolumn">
 		$commonside
+		 <div class="sideitem">
+		 
+		  <h6>Dashboard</h6>
+		  <div align="center">
+		   <font size="+1"><?= $projectInfo->dashboard_liveness() ?></font><br/>
+		   <?= $projectInfo->dashboard_bugs_lights() ?><br/>
+		   <?= $projectInfo->dashboard_news_lights() ?>
+		  </div>
+		 </div>
+		
 		<div class="sideitem">
 			<h6>Developer Links</h6>
 			<ul>

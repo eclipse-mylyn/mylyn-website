df16ebd0afac7fb01cb13ecc37fb3067f8e4fbbb
mkersten <mkersten>
2006-02-23 21:45:31 +0000

    Progress on: long: web site update

--- start.php
+++ start.php
@@ -46,45 +46,28 @@
 		</div>      
 			<p>In addition to providing personal task management, Mylar has the following 
 task/issue/bug/report repository integration.</p>
-        <table cellSpacing="2" cellPadding="6" width="100%" border="0" style="border-collapse: collapse" bordercolor="#FFFFFF" bordercolorlight="#FFFFFF" bordercolordark="#FFFFFF" bgcolor="#FFFFFF" id="table1">
-          <tr>
-            <td class="smallParagraph" align="center" valign="top" bgcolor="#7A79A7">
-            <b><font color="#FFFFFF">Integration</font></b></td>
-            <td class="smallParagraph" align="center" valign="top" bgcolor="#7A79A7">
-            <b><font color="#FFFFFF">Description</font></b></td>
-            <td class="smallParagraph" align="center" valign="top" bgcolor="#7A79A7">
-            <b><font color="#FFFFFF">Current support</font></b></td>
-          </tr>
-          <tr>
-            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
-            Web only</td>
-            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
-            Reports
-			<a href="http://www.eclipse.org/mylar/doc/faq.php#repositories-unsupported">
-			linked</a> via drag-and-drop, editing via embedded 
-			browser</td>
-            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
-            Any web repository</td>
-          </tr>
-          <tr>
-            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
-            Query support</td>
-            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
-            Repository queries in task list view</td>
-            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
-            JIRA</td>
-          </tr>
-          <tr>
-            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
-            Full integration</td>
-            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
-            Authoring of reports within native Eclipse editor, 
-			offline editing, incoming/outgoing status, compare and 
-			notifications. </td>
-            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
-            Bugzilla</td>
-          </tr>
-          </table>
+
+        <div class="homeitem3col">
+			<h3>Levels of Repository Integration</h3>
+			<ul>
+				<li>
+                    Web only: Reports
+					<a href="http://www.eclipse.org/mylar/doc/faq.php#repositories-unsupported">
+					linked</a> via drag-and-drop, editing via embedded 
+					browser</td> (any web-based repository).
+                </li>
+				<li>
+                    Query support: Repository queries in task list view (JIRA)
+                </li>
+				<li>
+                    Full integration: Authoring of reports within native Eclipse editor, 
+				offline editing, incoming/outgoing status, compare and 
+				notifications. </td>
+	            <td class="smallParagraph" align="left" valign="top" bgcolor="#F1F3F5">
+	            Bugzilla
+                </li>
+			</ul>
+		</div> 
 	</div>
 	
 	

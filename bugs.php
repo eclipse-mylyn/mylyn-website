a99da22200f061a9e2eae6c084deea511ddbfdc2
mkersten <mkersten>
2006-02-24 19:41:18 +0000

    Progress on: long: web site update

--- bugs.php
+++ bugs.php
@@ -1,18 +1,23 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT']."/eclipse.org-common/system/app.class.php");   
+    require_once($_SERVER['DOCUMENT_ROOT']."/eclipse.org-common/system/nav.class.php");   
+	require_once($_SERVER['DOCUMENT_ROOT']."/eclipse.org-common/system/menu.class.php");  
+	require_once($_SERVER['DOCUMENT_ROOT']."/projects/common/bug.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/projects/common/project-info.class.php");    
+	require_once($_SERVER['DOCUMENT_ROOT']."/projects/common/project_bugs.class.php");
+	include("_sideCommon.php");
 
-	#*****************************************************************************
-	#
-	#****************************************************************************
+    $App    = new App();    $Nav    = new Nav();    $Menu   = new Menu();   
+    ob_start();
+
+    include($App->getProjectCommon());   
+	$projectBugs = new ProjectBugs("technology.mylar");
 	
-	#
-	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "Mylar Bugs";
 	$pageKeywords	= "Mylar, Eclipse";
-	$pageAuthor		= "Mik Kersten";
-	
-	$projectBugs = new ProjectBugs("technology.mylar");
-
+	$pageAuthor		= "Mik Kersten";	
 ?>
+
 
 <div id="maincontent">
 	<div id="midcolumn">

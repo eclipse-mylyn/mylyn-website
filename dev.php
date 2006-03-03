743a269c31669f2a200cfc74a91c3284db236116
mkersten <mkersten>
2006-03-03 19:38:00 +0000

    Progress on: long: web site update

--- dev.php
+++ dev.php
@@ -10,6 +10,8 @@
     $App    = new App();    $Nav    = new Nav();    $Menu   = new Menu();   
     ob_start();
 
+
+	include("_sideDev.php");
     include($App->getProjectCommon());   
 	$projectInfo = new ProjectInfo("technology.mylar");
 	
@@ -71,23 +73,7 @@
            newsgroup: <?= $projectInfo->dashboard_news_lights() ?><br/>&nbsp;
           </div>
         </div>
-		<div class="sideitem">
-			<h6>Developer Links</h6>
-			<ul>
-				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=%5Bdiscussion%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-					Bugzilla discussions</a> of ideas, enhancements, and UI issues.
-					<br>&nbsp;
-				</li>
-				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=helpwanted&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-					Bugzilla reports marked helpwanted</a> are a starting point for those interested in contributing
-					<br>&nbsp;
-				</li>
-				<li><a href="http://www.eclipse.org/eclipse/development/index.php">Eclipse development page</a> documents
-					conventions and resources.<br>&nbsp;
-				</li>
-			</ul>
-		</div>
-        
+        $devside
 	</div>
 </div>
 

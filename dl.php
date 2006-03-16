b286c2fac8c5d5c7d6382a9ec218345339f664eb
mkersten <mkersten>
2006-03-16 17:31:16 +0000

    Progress on: long: web site update

--- dl.php
+++ dl.php
@@ -55,10 +55,10 @@
 			<h3>Mylar 0.4.x Update Sites</h3>
 			<ul>
 				<li>Eclipse 3.2M5: <b>download.eclipse.org/technology/mylar/update-site/e3.2</b>
-	        		<a href="http://download.eclipse.org/technology/mylar/update-site/mylar-site-local-e3.2.tgz">&nbsp;[zip]</a>
+	        		<a href="http://download.eclipse.org/technology/mylar/update-site/mylar-site-local-e3.2.tgz">&nbsp;[tgz]</a>
 	        	</li>
 	        	<li>Eclipse 3.1.x: <b>download.eclipse.org/technology/mylar/update-site/e3.1</b> 
-	        		<a href="http://download.eclipse.org/technology/mylar/update-site/mylar-site-local-e3.1.tgz">&nbsp;[zip]</a>
+	        		<a href="http://download.eclipse.org/technology/mylar/update-site/mylar-site-local-e3.1.tgz">&nbsp;[tgz]</a>
 	        	</li>
 			</ul>
 		</div>

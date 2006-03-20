c685f555bfe914969dc752d77b6803505172923b
mkersten <mkersten>
2006-03-20 23:12:13 +0000

    Progress on: long: web site update

--- sandbox/team.php
+++ sandbox/team.php
@@ -66,7 +66,7 @@
 				</li>
 				<li>
 				Ken Sueda (<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailassigned_to1=1&emailtype1=substring&email1=ksueda&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-				(39 reports resolved</a>, last active 2005-09-19)
+				39 reports resolved</a>, last active 2005-09-19)
 				</li>
 			</ul> 
 		</div>

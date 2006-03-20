028b908d673016499b9eaa3d707a2e9e9efb3404
mkersten <mkersten>
2006-03-20 23:55:15 +0000

    Progress on: long: web site update

--- sandbox/team.php
+++ sandbox/team.php
@@ -39,7 +39,7 @@
                 </li>
                 <li>
                 	Robert Elves (pending), (<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailassigned_to1=1&emailtype1=substring&email1=relves%40cs.ubc.ca&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-                	50 reports resolve</a>, last updated 2006-03-20)
+                	50 reports resolved</a>, last updated 2006-03-20)
                 </li>
 			</ul> 
 		</div>

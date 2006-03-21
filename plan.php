617c8958375a76f13ee36db7556cb1022ebbbea9
mkersten <mkersten>
2006-03-21 23:45:38 +0000

    Progress on: long: web site update

--- plan.php
+++ plan.php
@@ -26,7 +26,7 @@
 	<div id="midcolumn">
 		<div align="center"><h1>$pageTitle</h1></div>
 	<p>
-			Current priorities are represented by the <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&priority=P1&priority=P2&priority=P3&priority=P4&priority=P5&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
+			Current priorities are represented by the <a href="bugs.php">
 		open bugzilla reports</a>.
 		</p>
 	<div class="homeitem3col">

d9b31d69012e7ffacc35a038769bb704e7854a7e
mkersten <mkersten>
2006-03-22 16:31:40 +0000

    Progress on: long: web site update

--- team.php
+++ team.php
@@ -35,7 +35,7 @@
                     [resolved reports]</a>
                 </li>
                 <li>
-                	<a href="http://www.cs.ubc.ca/~murphy/">Gail Murphy</a>
+                	<a href="http://www.cs.ubc.ca/~murphy/">Gail Murphy</a>, project scientist
                 </li>
                 <li>
                 	Robert Elves (pending), <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailassigned_to1=1&emailtype1=substring&email1=relves%40cs.ubc.ca&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">

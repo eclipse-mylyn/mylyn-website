14f0ffe7999847ae45af1941d3c0fbd616f8ce9b
mkersten <mkersten>
2006-04-03 21:54:21 +0000

    Progress on: long: web site update

---
+++ sandbox/plan.php
@@ -0,0 +1,69 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Mylar Plan";
+	$pageKeywords	= "Mylar, Eclipse";
+	$pageAuthor		= "Mik Kersten";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<div id="maincontent">
+	<div id="midcolumn">
+		<div align="center"><h1>$pageTitle</h1></div>
+	<p>
+			Current priorities are represented by the <a href="bugs.php">
+		open bugzilla reports</a>.
+		</p>
+	<div class="homeitem3col">
+		<h3>Release Themes</h3>
+		<ul>
+			<li>Mylar 0.5: make tasks a first class abstraction, generic task repository support,
+				integration between tasks and contexts
+			</li>
+			<li>Mylar 0.6 (Callisto): provisional API stability, make context more first class
+				to facilitate sharing and reuse
+			</li>
+			<li>...</li>
+			<li>Mylar 1.0: tools project quality and API contract
+			</li>
+		</ul> 
+	</div>
+
+	<table>
+		<tr>
+			<td>foo</td>
+		</tr>
+	</table>
+
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	
+	</div>  
+	
+	<div id="rightcolumn">
+		$commonside
+	</div>
+
+</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>

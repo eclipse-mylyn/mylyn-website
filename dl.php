e2f6accfd7509f1eda373cc66571bc7af8d0b02e
mkersten <mkersten>
2006-04-10 19:52:01 +0000

    Progress on: long: web site update

--- dl.php
+++ dl.php
@@ -75,7 +75,7 @@
 			</ul>
 		</div>
 
-		<p><b>Supported platforms</b>: Eclipse 3.2M6 and later (our focus) and Eclipse 3.1.
+		<p><b>Supported platforms</b>: Eclipse 3.2M6 and later (recommended) and Eclipse 3.1.
 		Need <a href="http://java.sun.com/j2se/1.5.0/download.jsp">JRE 5.0</a> or later.  Mylar is developed on Windows; refer to the following queries for open bugs
 		with: 
 			<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr&short_desc=linux+gtk&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">

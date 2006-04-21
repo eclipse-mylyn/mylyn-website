2dd53eab715aff9ef29ea7588c4350e84e77b4f7
mkersten <mkersten>
2006-04-21 01:22:37 +0000

    Progress on: long: web site update

--- _sideNews.php
+++ _sideNews.php
@@ -3,6 +3,9 @@
 		<div class="sideitem">
 			<h6>News & Blogs</h6>
 			<ul>
+				<li>Riyad Kalla: <a href="http://www.eclipsezone.com/articles/mylar/">
+				Revolutionize your productivity with Eclipse Mylar</a>
+				<span class="dates">(Apr. 10, 2006)</span><br>&nbsp;</li>
 				<li>Eugene Kuleshov: <a href="http://jroller.com/page/eu?entry=jira_support_in_mylar">
 				Jira support in Mylar</a>
 				<span class="dates">(Feb. 23, 2006)</span><br>&nbsp;</li>
@@ -14,10 +17,10 @@
 				<span class="dates">(Nov. 7, 2005)</span><br>&nbsp;</li>
 				<li>Eugene Kuleshov: <a href="http://jroller.com/page/eu?entry=the_most_addictive_eclipse_plugin">The most addictive Eclipse plugin ever</a>
 				<span class="dates">(Nov. 1, 2005)</span><br>&nbsp;</li>
-				<li>First public release, 0.4<br><span class="dates">(Oct. 28, 2005)</span><br>&nbsp;</li>
 				<li>Ed Burnette: <a href="http://www.eclipsezone.com/eclipse/forums/t20933.html">Mylar is the next killer app for Eclipse</a>
 				<span class="dates">(July 28, 2005)</span><br>&nbsp;</li>
 				<!--
+				<li>First public release, 0.4<br><span class="dates">(Oct. 28, 2005)</span><br>&nbsp;</li>
 				<li>Eclipse.org project created<br><span class="dates">(June 13, 2005)</span></li>
 				<li>First user study preview release<br><span class="dates">(July 11, 2005)</span><br>&nbsp;</li>
 				-->

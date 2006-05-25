beb4d4d8b2826f9f23e2aeb03c41b172e04f95a2
mkersten <mkersten>
2006-05-25 15:40:38 +0000

    Progress on: long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -7,7 +7,7 @@
 			(May 22, 2006)</span><br>&nbsp;</li>
 			<li>
 			<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr&short_desc=%5Bconnector%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-			Vote</a> for connectors
+			Vote</a> for connectors <img border=0 src="images/vote.gif">
 		</ul> 
 	</div> 
 EOHTML

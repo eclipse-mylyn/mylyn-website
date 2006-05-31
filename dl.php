44455391c5ccaacd7820229a3eba41b7cd7f7354
mkersten <mkersten>
2006-05-31 21:45:01 +0000

    Progress on: long: web site update

--- dl.php
+++ dl.php
@@ -32,7 +32,7 @@
         
         <table border="0">
         	<tr>
-        		<td valign="top"><div id="body">
+        		<td valign="top"><div class="body">
 		        <img src="images/feature.gif">
 		        Mylar can be installed as a standalone Task List for managing personal tasks, with the Focused UI support for the Eclipse SDK
 		        to reduce information overload, and with one or more task <a href="start.php">repository connectors</a>.  All features are optional

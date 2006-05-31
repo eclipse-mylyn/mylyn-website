d34613514906b912fb20139f6f8698e1382dbfc3
mkersten <mkersten>
2006-05-31 22:16:29 +0000

    Progress on: long: web site update

--- dl.php
+++ dl.php
@@ -28,24 +28,28 @@
 
 <div id="maincontent">
 	<div id="midcolumn">
-		<div align="center"><h1>$pageTitle</h1></div>
+		<div align="center">
+			<h1>$pageTitle</h1>
+		</div>
         
-        <table border="0">
-        	<tr>
-        		<td valign="top"><p>
-		        <img src="images/feature.gif">
-		        Mylar can be installed as a standalone Task List for managing personal tasks, with the Focused UI support for the Eclipse SDK
-		        to reduce information overload, and with one or more task <a href="start.php">repository connectors</a>.  All features are optional
-		        unless indicated otherwise by the update manager.  Note that installing the JIRA Connector involves downloading a feature
-				from jira-dashboard.tigris.org.
-				Use <b><i>"Search for new features.."</i></b> to <a href="http://www.eclipse.org/mylar/doc/faq.php#install">install updates</a>.
-		        </p>
-		        </td>
-		     <td>
-				<img src="/mylar/images/install-overview.gif">
-		     </td>
-		    </tr>
-		</table>
+        <div align="left">
+	        <table border="0">
+	        	<tr>
+	        		<td valign="top"><p>
+			        <img src="images/feature.gif">
+			        Mylar can be installed as a standalone Task List for managing personal tasks, with the Focused UI support for the Eclipse SDK
+			        to reduce information overload, and with one or more task <a href="start.php">repository connectors</a>.  All features are optional
+			        unless indicated otherwise by the update manager.  Note that installing the JIRA Connector involves downloading a feature
+					from jira-dashboard.tigris.org.
+					Use <b><i>"Search for new features.."</i></b> to <a href="http://www.eclipse.org/mylar/doc/faq.php#install">install updates</a>.
+			        </p>
+			        </td>
+			     <td>
+					<img src="/mylar/images/install-overview.gif">
+			     </td>
+			    </tr>
+			</table>
+		</div>
         
         <div class="homeitem3col">
 			<h3>Mylar 0.5.x Update Sites</h3>
@@ -67,28 +71,29 @@
 			</ul>
 		</div>
 
-		<p><b>Supported platforms</b>: Eclipse 3.2M6 and later (recommended) and Eclipse 3.1.
-		Need <a href="http://java.sun.com/j2se/1.5.0/download.jsp">JRE 5.0</a> or later.  
-		<!--
-		Mylar is developed on Windows; refer to the following queries for open bugs
-		with: 
-			<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr&short_desc=linux+gtk&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-			Linux</a>, 
-			<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr&short_desc=mac&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-			MacOS</a>.
-		-->
-		<!--
-		There are no additional system performance requirements, but see the FAQ for <a href="http://www.eclipse.org/mylar/doc/faq.php#performance-summary">performance details</a>.</p>
-		--> 
-		</p>
-		<p><b>Supported repositories</b>: Bugzilla 2.16-2.22, JIRA 3.1.1 and later.
-		</p>
- 		
-		<b>Expectations</b>: Mylar 0.5 is targeted at early adopters.  The technology and 
-		UI continue to evolve raplidly based on user community feedback.  New builds are 
-		made available weekly, and contain fixes for key issues reported the previous week.
-
+		<div align="left">
+			<p><b>Supported platforms</b>: Eclipse 3.2M6 and later (recommended) and Eclipse 3.1.
+				Need <a href="http://java.sun.com/j2se/1.5.0/download.jsp">JRE 5.0</a> or later.  
+				<!--
+					Mylar is developed on Windows; refer to the following queries for open bugs
+					with: 
+						<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr&short_desc=linux+gtk&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
+						Linux</a>, 
+						<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr&short_desc=mac&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
+						MacOS</a>.
+					There are no additional system performance requirements, but see the FAQ for <a href="http://www.eclipse.org/mylar/doc/faq.php#performance-summary">performance details</a>.</p>
+				--> 
+			</p>
+			<p>
+				<b>Supported repositories</b>: Bugzilla 2.16-2.22, JIRA 3.1.1 and later.
+			</p>
+	 		
+			<b>Expectations</b>: Mylar 0.5 is targeted at early adopters.  The technology and 
+			UI continue to evolve raplidly based on user community feedback.  New builds are 
+			made available weekly, and contain fixes for key issues reported the previous week.
+		</div>
 		<p>&nbsp;</p>
+		
 	</div>
 
 	<div id="rightcolumn">
--- index.php
+++ index.php
@@ -24,55 +24,58 @@
   
 <div id="maincontent">
 	<div id="midcolumn">
-	
-		<div align="center"><h1>$pageTitle</h1></div>
+		<div align="center">
+			<h1>$pageTitle</h1>
+		</div>
 		
-		<table border="0" cellpadding="4">
-          <tr>
-          	<td valign="top"><img border=0 src="images/screenshot-mylar-small.gif"></td>
-          	<td valign="top">&nbsp;</td>
-            <td valign="top">
-				<p>
-				Mylar is a task focused UI for Eclipse that makes working with very large workspaces as easy as working with small ones. It supports task management and monitors your work activity to identify information relevant to the task-at-hand. Mylar uses this task context to focus the Eclipse UI on the interesting information, hide the uninteresting, and automatically find what's related. This puts the information you need to get work done at your fingertips and improves productivity by reducing searching, scrolling, and navigation. By making task context explicit Mylar also facilitates multitasking, planning, reusing past efforts, and sharing expertise.
-				</p>
-		  	</td>
-          </tr>
-        </table>
-
+		<div align="left">
+			<table border="0" cellpadding="4">
+	          <tr> 
+	          	<td valign="top"><img border=0 src="images/screenshot-mylar-small.gif"></td>
+	          	<td valign="top">&nbsp;</td>
+	            <td valign="top">
+					<p>
+					Mylar is a task focused UI for Eclipse that makes working with very large workspaces as easy as working with small ones. It supports task management and monitors your work activity to identify information relevant to the task-at-hand. Mylar uses this task context to focus the Eclipse UI on the interesting information, hide the uninteresting, and automatically find what's related. This puts the information you need to get work done at your fingertips and improves productivity by reducing searching, scrolling, and navigation. By making task context explicit Mylar also facilitates multitasking, planning, reusing past efforts, and sharing expertise.
+					</p>
+			  	</td>
+	          </tr>
+	        </table>
+        </div>
+	 
 		<div class="homeitem">
 			<h3>Community</h3>
 			<ul>
 				<li>
-                    <a href="news://news.eclipse.org/eclipse.technology.mylar">
+	                <a href="news://news.eclipse.org/eclipse.technology.mylar">
 					mylar newsgroup:</a> user discussions and minor release announcements
 					<a href="http://dev.eclipse.org/newslists/news.eclipse.technology.mylar/maillist.html">[archive]</a></li>
 				<li>
-                    <a href="http://dev.eclipse.org/mailman/listinfo/mylar-announce">
+	                <a href="http://dev.eclipse.org/mailman/listinfo/mylar-announce">
 					mylar-announce@eclipse.org:</a> major release and event announcements
 					[<a href="http://dev.eclipse.org/mhonarc/lists/mylar-announce/maillist.html">archive</a>]</li>
 				<li>
-                    <a href="http://dev.eclipse.org/mailman/listinfo/mylar-dev">
+	                <a href="http://dev.eclipse.org/mailman/listinfo/mylar-dev">
 					mylar-dev@eclipse.org:</a> developer discussions and planning
 					<a href="http://dev.eclipse.org/mhonarc/lists/mylar-dev/maillist.html">[archive]</a></li>
 				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=%5Bdiscussion%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
 					Bugzilla discussions</a> of ideas, enhancements, and UI issues.</li>
 			</ul>
 		</div>
-
+		
 		<div class="homeitem">
 			<h3>Recent and Upcoming Events</h3>
 			<ul>
 				<li>
-                    <a href="http://www.eclipsecon.org/2006/Sub.do?id=164">
-                    Mylar Talk</a> at EclipseCon 2006</a>, March 20-23, Santa Clara, USA
-                	<br>
-                	<a href="http://www.eclipsecon.org/2006/Sub.do?id=164"><img border=0 
+	                <a href="http://www.eclipsecon.org/2006/Sub.do?id=164">
+	                Mylar Talk</a> at EclipseCon 2006</a>, March 20-23, Santa Clara, USA
+	            	<br>
+	            	<a href="http://www.eclipsecon.org/2006/Sub.do?id=164"><img border=0 
 					 src="http://www.eclipsecon.org/2006/images/banner132x38.gif"/></a>
-                </li>
+	            </li>
 			</ul>
 		</div> 
 		
-	</div> 
+	</div>
 
 	<div id="rightcolumn">
 		$commonside

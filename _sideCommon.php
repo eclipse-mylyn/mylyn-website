d2a01e4eb4c1fcab99c28d5498a9456f22ef43b5
mkersten <mkersten>
2006-07-08 02:27:43 +0000

    Progress on: long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -5,7 +5,7 @@
 		<ul>
 			<li>
 				<a href="doc/new.php">Mylar 0.6.0</a><br><span class="dates"> 
-				(June 30, 2006)</span>
+				(June 30, 2006)</span><br>&nbsp;
 			</li>
 			<li>
 				<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr&short_desc=%5Bconnector%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">

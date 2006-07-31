b4bae723998aef5348e237f507edd9f40ec6e74e
mkersten <mkersten>
2006-07-31 23:46:16 +0000

    Progress on: long: web site update

--- dl.php
+++ dl.php
@@ -74,7 +74,7 @@
 		</div>
 
 		<div align="left">
-			<p><b>Supported platforms</b>: Eclipse SDK 3.2 and 3.1, <a href="http://java.sun.com/j2se/1.5.0/download.jsp">JRE 5.0</a> or later required.
+			<p><b>Supported platforms</b>: <a href="http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops/R-3.2-200606291905/eclipse-SDK-3.2-win32.zip">Eclipse SDK</a> 3.2 and 3.1, <a href="http://java.sun.com/j2se/1.5.0/download.jsp">JRE 5.0</a> or later required.
 				<!--
 					Mylar is developed on Windows; refer to the following queries for open bugs
 					with: 

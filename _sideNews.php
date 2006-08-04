be5592f3c1efe1d1fc6dc909a1433c0b50093710
mkersten <mkersten>
2006-08-04 08:43:11 +0000

    Progress on: long: web site update

--- _sideNews.php
+++ _sideNews.php
@@ -4,6 +4,11 @@
 			<h6><a href="archive.php">Blogs & Articles...</a></a></h6>
 			<ul>
 				<li>
+					Eugene Kuleshov: <a href="http://jroller.com/page/eu?entry=linking_mylar_with_google_code">
+					Link Mylar with Google Code Hosting...</a>
+					<span class="dates">(Aug 4, 2006)</span><br>&nbsp;
+				</li>
+				<li>
 					R.J. Lorimer: <a href="http://www.eclipsezone.com/eclipse/forums/t75928.html">Mylar: the Forgotten Feature?</a>
 					<span class="dates">(Jul 9, 2006)</span><br>&nbsp;
 				</li>
@@ -15,30 +20,26 @@
 				<li>
 					Riyad Kalla: <a href="http://www.eclipsezone.com/articles/mylar/">
 					Revolutionize your productivity with Eclipse Mylar</a>
-					<span class="dates">(Apr. 10, 2006)</span><br>&nbsp;
+					<span class="dates">(Apr 10, 2006)</span><br>&nbsp;
 				</li>
 				<li>
 					Eugene Kuleshov: <a href="http://swik.net/Eclipse/Euxx/Mylar's+ultra-fast+release+cycles/c4nm">
 					Mylar's ultra-fast release cycles</a>
-					<span class="dates">(Apr. 8, 2006)</span><br>&nbsp;
+					<span class="dates">(Apr 8, 2006)</span><br>&nbsp;
 				</li>
 				<li>
 					Gedanken Verloren: <a href="http://www.jroller.com/page/musk?entry=eclipse_killer_plugin_mylar">
 					Eclipse Killer Plugin Mylar</a>
-					<span class="dates">(Nov. 14, 2005)</span><br>&nbsp;
+					<span class="dates">(Nov 14, 2005)</span><br>&nbsp;
 				</li>
 				<li>
 					Kirill Grouchnikov: <a href="http://weblogs.java.net/blog/kirillcool/archive/2005/11/mylar_a_very_us.html">
 					A very useful Eclipse plugin</a>
-					<span class="dates">(Nov. 7, 2005)</span><br>&nbsp;
-				</li>
-				<li>
-					Eugene Kuleshov: <a href="http://jroller.com/page/eu?entry=the_most_addictive_eclipse_plugin">The most addictive Eclipse plugin ever</a>
-					<span class="dates">(Nov. 1, 2005)</span><br>&nbsp;
+					<span class="dates">(Nov 7, 2005)</span><br>&nbsp;
 				</li>
 				<li> 
 					Ed Burnette: <a href="http://www.eclipsezone.com/eclipse/forums/t20933.html">Mylar is the next killer app for Eclipse</a>
-					<span class="dates">(July 28, 2005)</span><br>
+					<span class="dates">(Jul 28, 2005)</span><br>
 				</li>
 			</ul>
 		</div>

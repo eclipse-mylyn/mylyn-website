0b22d54114dfe6b9dc0e42de9fe60e0609992818
mkersten <mkersten>
2006-08-29 17:56:25 +0000

    Progress on: long: web site update

--- publications.php
+++ publications.php
@@ -17,58 +17,62 @@
 	$html = <<<EOHTML
 
 <div id="maincontent">
+	<div align="center"><h1>$pageTitle</h1></div>
+	
+	
 	<div id="midcolumn">
-		<div align="center"><h1>$pageTitle</h1></div>
 		
-	<div class="homeitem3col">
-		<h3>Presentations</h3>
-		<ul>
-            <li>
-            	March 2006, EclipseCon talk: 
-            	<a href="publications/2006-03-mylar-eclipsecon.ppt">
-            	Mylar: a Task Focused UI for Eclipse [PPT]</a>
-            </li>
-            <li>
-            	March 2005, EclipseCon talk: 
-            	<a href="publications/2005-03-mylar-eclipsecon-web.ppt">
-            	Mylar: a degree-of-interest model for Eclipse [PPT]</a>
-            </li>
-		</ul> 
-	</div>
+		<div class="homeitem3col">
+			<h3>Presentations</h3>
+			<ul>
+	            <li>
+	            	March 2006, EclipseCon talk: 
+	            	<a href="publications/2006-03-mylar-eclipsecon.ppt">
+	            	Mylar: a Task Focused UI for Eclipse [PPT]</a>
+	            </li>
+	            <li>
+	            	March 2005, EclipseCon talk: 
+	            	<a href="publications/2005-03-mylar-eclipsecon-web.ppt">
+	            	Mylar: a degree-of-interest model for Eclipse [PPT]</a>
+	            </li>
+			</ul> 
+		</div>
 		
-	<div class="homeitem3col">
-		<h3>Publications</h3>
-		<ul>
-            <li>
-            	March 2005, IEEE Software article: 
-            	Murphy, Kersten and Findlater. <a href="publications/2005-03-mylar-aosd-web.ppt">
-            	How are Java software developers using the Eclipse IDE?</a>&nbsp;
-				<a href="publications/2006-07-mylar-ieee.pdf"><img border=0 src="images/icon-pdf.gif"></a>
-            </li>
-			<li>
-                May 2005, eclipse.org 
-                <a href="publications/2005-04-mylar-proposal.html">
-                Project creation review</a>
-                <a href="publications/2005-05-mylar-creation-review.ppt">
-				[PPT]</a>
-            </li>
-            <li>
-            	March 2005, AOSD research paper: 
-            	<a href="publications/2005-03-mylar-aosd-web.ppt">
-            	Mylar: a degree-of-interest model for IDEs [PPT]</a>&nbsp;
-				<a href="publications/2005-03-mylar-aosd.pdf">[PDF]</a>
-            </li>
-		</ul> 
-	</div>
+		<div class="homeitem3col">
+			<h3>Publications</h3>
+			<ul>
+	            <li>
+	            	March 2005, IEEE Software article: 
+	            	Murphy, Kersten and Findlater. <a href="publications/2005-03-mylar-aosd-web.ppt">
+	            	How are Java software developers using the Eclipse IDE?</a>&nbsp;
+					<a href="publications/2006-07-mylar-ieee.pdf"><img border=0 src="images/icon-pdf.gif"></a>
+	            </li>
+				<li>
+	                May 2005, eclipse.org 
+	                <a href="publications/2005-04-mylar-proposal.html">
+	                Project creation review</a>
+	                <a href="publications/2005-05-mylar-creation-review.ppt">
+					[PPT]</a>
+	            </li>
+	            <li>
+	            	March 2005, AOSD research paper: 
+	            	<a href="publications/2005-03-mylar-aosd-web.ppt">
+	            	Mylar: a degree-of-interest model for IDEs [PPT]</a>&nbsp;
+					<a href="publications/2005-03-mylar-aosd.pdf">[PDF]</a>
+	            </li>
+			</ul> 
+		</div>
 		<p>
 		
-	Also see <a href="http://wiki.eclipse.org/index.php/Mylar_Related_Research_Projects">Related Research Projects</a>
-	for links to other research papers related to Mylar.
-	</p>
-	<p>* <font size=-2>Copyright: © 2006 IEEE. Personal use of this material is permitted. However, permission to reprint/republish
-	this material for advertising or promotional purposes or for creating new collective works for resale or redistribution
-	to servers or lists, or to reuse any copyrighted component of this work in other works must be obtained from the IEEE.</font>
-	</p>
+		Also see <a href="http://wiki.eclipse.org/index.php/Mylar_Related_Research_Projects">Related Research Projects</a>
+		for links to other research papers related to Mylar.
+		</p>
+		<p>* <font size=-2>Copyright: © 2006 IEEE. Personal use of this material is permitted. However, permission to reprint/republish
+		this material for advertising or promotional purposes or for creating new collective works for resale or redistribution
+		to servers or lists, or to reuse any copyrighted component of this work in other works must be obtained from the IEEE.</font>
+		</p>
+	
+	</div>
 	
 	<div id="rightcolumn">
 		$commonside

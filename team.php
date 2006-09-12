58b29760374faeb43ec5eeffb73c982000826e2a
mkersten <mkersten>
2006-09-12 05:15:31 +0000

    Progress on: long: web site update

--- team.php
+++ team.php
@@ -25,9 +25,9 @@
 <div id="maincontent">
 	<div id="midcolumn">
 		<!--	<div align="center"><h1>$pageTitle</h1></div> -->
+		<p>In order of bugs resolved. <i>Last updated: 2006-07-21</i></p>
 		<div class="homeitem3col">
-		    <p>In order of bugs resolved. <i>Last updated: 2006-07-21</i></p>
-			<h3>Committers</h3>
+		    <h3>Committers</h3>
 			<ul>
 				<li>
                     <a href="http://kerstens.org/mik">Mik Kersten</a>, project lead <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&emailassigned_to1=1&emailtype1=substring&email1=mik.kersten&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">

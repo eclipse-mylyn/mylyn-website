b938f6cd0f6eadf55390ab6a9f9eb8e594a9410f
mkersten <mkersten>
2006-10-11 04:01:45 +0000

    Progress on: long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -16,6 +16,35 @@
 				<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-ratelink-lid-560.html">
 				Comment on Mylar</a> at EPIC
 			</li>
+			
+			<li>
+			 <form method="post" action="Web_Links.html">
+				<table align="center" border="0" width="175" cellspacing="0" cellpadding="0">
+				<tr><td align="center"><b>Rate Mylar at EPIC</b></a></td></tr>
+				<tr><td>
+				<table border="0" cellspacing="0" cellpadding="0" align="center">
+				<tr><td valign="top">
+				<select name="rating">
+				<option selected>10</option>
+				<option>9</option>
+				<option>8</option>
+				<option>7</option>
+				<option>6</option>
+				<option>5</option>
+				<option>4</option>
+				<option>3</option>
+				<option>2</option>
+				<option>1</option>
+				</select>
+				</td><td valign="top">
+				<input type="hidden" name="ratinglid" value="560">
+				<input type="hidden" name="ratinguser" value="outside">
+				<input type="hidden" name="req" value="addrating">
+				<input type="submit" value="Submit">
+				</td></tr></table>
+				</td></tr></table>
+				</form>
+			</li>
 		</ul> 
 		
 	</div> 

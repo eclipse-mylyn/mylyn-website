6cf0d0e221b5b71c21ffa6fb05b0a2f21e15a888
mkersten <mkersten>
2006-10-11 04:17:14 +0000

    Progress on: long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -19,27 +19,27 @@
 			-->
 			
 			<li>
-			 <form method="post" action="Web_Links.html">
+			 <form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
 						<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">Rate Mylar at EPIC</a>
 						<br>
 						
-								<select name="rating">
-								<option selected>10</option>
-								<option>9</option>
-								<option>8</option>
-								<option>7</option>
-								<option>6</option>
-								<option>5</option>
-								<option>4</option>
-								<option>3</option>
-								<option>2</option>
-								<option>1</option>
-								</select>
+						<select name="rating">
+						<option selected>10</option>
+						<option>9</option>
+						<option>8</option>
+						<option>7</option>
+						<option>6</option>
+						<option>5</option>
+						<option>4</option>
+						<option>3</option>
+						<option>2</option>
+						<option>1</option>
+						</select>
 
-							<input type="hidden" name="ratinglid" value="560">
-							<input type="hidden" name="ratinguser" value="outside">
-							<input type="hidden" name="req" value="addrating">
-							<input type="submit" value="Submit">
+						<input type="hidden" name="ratinglid" value="560">
+						<input type="hidden" name="ratinguser" value="outside">
+						<input type="hidden" name="req" value="addrating">
+						<input type="submit" value="Submit">
 				</form>
 			</li>
 			

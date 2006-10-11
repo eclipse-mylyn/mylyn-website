0ff9808b7da256253c7db467078c18a7ef9cfc7a
mkersten <mkersten>
2006-10-11 04:06:50 +0000

    Progress on: long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -22,7 +22,7 @@
 			<li>
 			<br>&nbsp;
 			 <form method="post" action="Web_Links.html">
-				<table align="left" border="0" width="175" cellspacing="0" cellpadding="0">
+				<table align="left" border="0" cellspacing="0" cellpadding="0">
 				<tr><td align="left"><a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">Rate Mylar at EPIC</a></td></tr>
 				<tr><td>
 				<table border="0" cellspacing="0" cellpadding="0" align="left">

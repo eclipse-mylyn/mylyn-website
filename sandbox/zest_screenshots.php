d0739696da0c9ae3b37044d942d2cddb8ec50941
ibull <ibull>
2006-11-23 19:33:12 +0000

    Incomplete - bug 152810: Update Website https://bugs.eclipse.org/bugs/show_bug.cgi?id=152810

--- sandbox/zest_screenshots.php
+++ sandbox/zest_screenshots.php
@@ -6,7 +6,7 @@
 	
 	#
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Zest: The Eclipse Visualization Toolkit";
+	$pageTitle 		= "Zest Screenshot Gallery";
 	$pageKeywords	= "Mylar, Eclipse, Zest, Visualization";
 	$pageAuthor		= "Ian Bull";
 	
@@ -23,41 +23,6 @@
 	$html = <<<EOHTML
 
 <div id="maincontent">
-	<div id="midcolumn">
-		<div align="center"><h1>$pageTitle</h1></div>
-       <p >
-       	<table border="0" cellpadding="4">
-          <tr>
-          	<td valign="top"><img border=0 src="images/zest_screen.jpg"></td>
-          	<td valign="top">&nbsp;</td>
-            <td valign="top">
-	     <p>
-		<strong>Zest: The Eclipse Visualization Toolkit,</strong> is a set of 
-		visualization components built for Eclipse.  
-		Zest is a component of the Mylar project that 
-		can be used independently of the Mylar IDE 
-		support. The entire Zest library has been
-		developed in SWT and integrates seamlessly within Eclipse because of its
-		recognized design.   
-		<P>
-		Zest has been modeled after JFace, and all the Zest views
-		conform to the same standards and conventions as existing Eclipse views.  This
-		means that the providers, actions and listeners used within existing
-		applications can be leveraged within Zest.  
-		Zest is a visualization toolkit with a growing
-		number of views, all of which aim to be easy to program against. We are actively
-                looking for new visualizations to include in the toolkit.
-		
-		<p>The Zest project also contains a graph layout package which can be used 
-		independently.  The graph layout package can be used within existing
-		Java applications (SWT or AWT) to provider layout locations for a set of
-		entities and relationships.
-		</p>
-		<A HREF="http://wiki.eclipse.org/index.php/Mylar_Zest_Visualization">Please visit our Wiki for more information.</A>
-  	  </td>
-          </tr>
-        </table>
-	
 		<div class="homeitem3col">
 		<h3>Information</h3>
 		<ul>
@@ -97,37 +62,7 @@
 		</ul>
 		</div>  
 	</div>
-	<div id="rightcolumn">
-		$commonside
-		<center>
-		<a href="zest_screenshots.php">
-		<img src="images/zest_screen1_th.jpg"/><BR>
-		Screenshots
-		</a>
-		<center>
-	</div>
-	<div id="rightcolumn">
-		$commonside
-		<div class="sideitem">
-			<h6>Zest Viewers</h6>
-			<ul>
-				<li>Static Graph Viewer</li>
-                <li>Nested Graph Viewer</li>
-			</ul>
-		</div>
-	</div>
-	<div id="rightcolumn">
-		$commonside
-		<div class="sideitem">
-			<h6><A HREF="http://wiki.eclipse.org/index.php/Mylar_Zest_Visualization#Layout_Algorithms">Layout Algorithms</A></h6>
-			<ul>
-				<li>Spring Layout Algorithm</li>
-                                <li>Tree Layout Algorithm</li>
-                                <li>Radial Layout Algorithm</li>
-                                <li>Grid Layout Algorithm</li>
-			</ul>
-		</div>
-	</div>
+	
 </div>
 
 

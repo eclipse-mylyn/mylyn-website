6af122b6f7edfb5898c7772c850021ccdecce12e
mkersten <mkersten>
2006-11-28 17:24:23 +0000

    Incomplete - long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -4,7 +4,7 @@
 		<h6>Releases</h6>
 		<ul>
 			<li>
-				<a href="doc/new.php">Mylar 0.9.1 (RC1)</a>
+				<a href="doc/new.php">Mylar 0.9.1</a>
 				<span class="dates">(Nov 28)</span>
 			</li><br>&nbsp;
 			<!--

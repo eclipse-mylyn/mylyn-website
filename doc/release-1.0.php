9223b7e8c860686b78074412b2469b619dd188eb
mkersten <mkersten>
2006-12-11 08:29:59 +0000

    long: web site update

---
+++ doc/release-1.0.php
@@ -0,0 +1,40 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	#****************************************************************************
+	
+	#
+	$pageTitle 		= "Mylar New & Noteworthy";
+	$pageKeywords	= "Mylar, Eclipse";
+	$pageAuthor		= "Mik Kersten";
+	
+	$filename = $_SERVER['DOCUMENT_ROOT'] . "/mylar/doc/new.html";
+    $dataFile = fopen( $filename, "r" ) ;
+    if ( $dataFile )  {
+    	while (!feof($dataFile))   {
+        	$contents .= fgets($dataFile, 4096);
+    	}
+    	fclose($dataFile);
+    } else  {
+    	die( "fopen failed for $filename" ) ;
+    }
+
+	# Paste your HTML content between the EOHTML markers!	
+	
+	include("_sideCommon.php");
+		
+	$html = <<<EOHTML
+
+<div id="midcolumn" style="width:78%">
+     $contents
+</div>
+</div>
+
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>

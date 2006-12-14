872a2bf7c5bcf1b5aa7953c769255b7d01cfe560
mkersten <mkersten>
2006-12-14 21:26:58 +0000

    long: web site update

--- _sideNews.php
+++ _sideNews.php
@@ -1,8 +1,9 @@
 <?php
     $newsside = <<<EOHTML
 		<div class="sideitem">
-		<font size=-1>
+		
 		<h6><a href="archive.php">News & Blogs...</a></a></h6>
+		<font size=-1>
 			Ed Burnette, ZDNet: <a href="http://blogs.zdnet.com/Burnette/?p=218&page=1">
 			Q&A with Mik Kersten, Mylar project lead</a>
 			<span class="dates">(Dec 13)</span><br>&nbsp;

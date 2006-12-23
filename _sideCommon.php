17c59159b93794931a1c1be8ee931dddafb86b84
mkersten <mkersten>
2006-12-23 01:29:40 +0000

    long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -4,7 +4,7 @@
 		<h6>Releases</h6>
 		<font size=1>
 			<a href="doc/new.php">Mylar 1.0.1</a>
-			<span class="dates">(Dec 22b)</span>
+			<span class="dates">(Dec 22)</span>
 		<!--
 		<br>
 			<i>1.0.1/3.3M4: Dec 22</i>-->

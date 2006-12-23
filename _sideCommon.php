4cbe40e083f1ef21cc8de93f5c6b786fe52fc1f1
mkersten <mkersten>
2006-12-23 01:22:58 +0000

    long: web site update

--- _sideCommon.php
+++ _sideCommon.php
@@ -3,10 +3,11 @@
 	<div class="sideitem">
 		<h6>Releases</h6>
 		<font size=1>
-			<a href="doc/new.php">Mylar 1.0.0</a>
-			<span class="dates">(Dec 11)</span>
+			<a href="doc/new.php">Mylar 1.0.1</a>
+			<span class="dates">(Dec 22b)</span>
+		<!--
 		<br>
-			<i>1.0.1/3.3M4: Dec 22</i>
+			<i>1.0.1/3.3M4: Dec 22</i>-->
 		</font>
 	</div> 
 EOHTML

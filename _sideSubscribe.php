c1e1b90fcc2c24ab412bb9b5f5f3ee5035c3fecb
mkersten <mkersten>
2007-02-20 01:55:22 +0000

    Version update

--- _sideSubscribe.php
+++ _sideSubscribe.php
@@ -1,9 +1,9 @@
 <?php
     $sideSubscribe = <<<EOHTML
 		<div class="sideitem">
-			<h6>Project Status</h6>
+			<h6>Announcements</h6>
 				<a href="https://dev.eclipse.org/mailman/listinfo/mylar-announce">
-					<img border=0 src="images/icon-subscribe"> Subscribe</a> to the announce list.
+					<img border=0 src="images/icon-subscribe.gif"> Subscribe</a> to the low-traffic announce list.
 		</div>
 EOHTML
 

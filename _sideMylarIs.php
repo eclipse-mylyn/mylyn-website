eb2b9e45df7344272606a1131f717e7d3216b0a3
mkersten <mkersten>
2007-02-20 20:41:59 +0000

    NEW - bug 174676: Broken download link https://bugs.eclipse.org/bugs/show_bug.cgi?id=174676

--- _sideMylarIs.php
+++ _sideMylarIs.php
@@ -1,10 +1,12 @@
 <?php
     $mylarIsSide = <<<EOHTML
-    	<!--
 		<div class="sideitem">
-			<h6>&quot;Mylar&quot; is</h6>
+			<h6>&quot;Mylar(r)&quot; is</h6>
 			<font size=1>
-				An aluminized film used to avoid blindness when staring at a solar eclipse.<br>&nbsp;<br>
+				A polyester film used to avoid blindness when staring at a solar eclipse.
+			</font>
+			<h6>&quot;Eclipse Mylar&quot; is</h6>
+			<font size=1>
 				A focused user interface used to avoid information blindness when staring at Eclipse.
 			</font>
 		</div> 

0376d68212062e1964b1c84fe9a3e48d45f5f0fe
mkersten <mkersten>
2007-02-20 02:35:53 +0000

    NEW - bug 174676: Broken download link https://bugs.eclipse.org/bugs/show_bug.cgi?id=174676

--- _sideFeedback.php
+++ _sideFeedback.php
@@ -3,10 +3,9 @@
 	<div class="sideitem">
 		<h6>Feedback</h6>
 		<font size=1>
-		<a href="bugs.php">
-		Vote</a> for connectors<br>
-		 <form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
-			<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">Rate and Comment via EPIC</a>
+		<a href="bugs.php">Vote for connectors</a>
+		<br><form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
+			<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">Comment via EPIC</a>
 			<select name="rating">
 			<option selected>10</option>
 			<option>9</option>
@@ -22,7 +21,7 @@
 			<input type="hidden" name="ratinglid" value="560">
 			<input type="hidden" name="ratinguser" value="outside">
 			<input type="hidden" name="req" value="addrating">
-			<input type="submit" value="Submit">
+			<input type="submit" value="Rate">
 			</form>
 		</font>
 	</div> 

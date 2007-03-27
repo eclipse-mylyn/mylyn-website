287f2636c91c6ca49d421a3af9a3e2d2ddd18105
ibull <ibull>
2007-03-27 19:36:15 +0000

    NEW - bug 152810: Update Website https://bugs.eclipse.org/bugs/show_bug.cgi?id=152810

---
+++ sandbox/zest/zest_snippets.php
@@ -0,0 +1,52 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Zest Screenshot Gallery";
+	$pageKeywords	= "Mylar, Eclipse, Zest, Visualization";
+	$pageAuthor		= "Ian Bull";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<div id="maincontent">
+
+		<div id="midcolumn">
+		<H1>Zest Snippets</H1>
+		Click on a snippet to see the code
+		</div>
+		
+		<div class="homeitem3col">
+
+		<h3>Zest Snippets</h3>
+		<center>
+		<P>
+		<BR>
+		<B>SWT Style Snippets</B>
+		<A HREF="GraphSnippet1.html">Simple graph with 3 nodes and 3 edges</A>
+		</center>
+		</div>  
+	</div>
+	
+</div>
+
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>

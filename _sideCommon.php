836fa749d809cae523c4cb99a98cbfaa1141385d
mkersten <mkersten>
2007-05-12 04:41:41 +0000

    NEW - bug 179813: Usage Summary editor improvements https://bugs.eclipse.org/bugs/show_bug.cgi?id=179813

--- _sideCommon.php
+++ _sideCommon.php
@@ -5,7 +5,7 @@
 		<font size=1>
 			<a href="doc/new.php">Mylar 2.0M2</a>
 			<span class="dates">(March 30)</span>
-			<br><i>Mylar 2.0M3 (May 11)</i>
+			<br><i>Mylar 2.0M3 (May 14)</i>
 		<!--
 		<br>
 			<i>1.0.1/3.3M4: Dec 22</i>-->

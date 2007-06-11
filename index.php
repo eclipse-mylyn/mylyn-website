86d728477611e851f72be6cf77984cb5335505cd
mkersten <mkersten>
2007-06-11 17:41:46 +0000

    NEW - bug 191406: rename Mylar project to Mylyn https://bugs.eclipse.org/bugs/show_bug.cgi?id=191406

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
 				<!--
 				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=%5Bdiscussion%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
 					Bugzilla discussions</a> of ideas, enhancements, and UI issues.</li>
-				-->
+				--> 
 			</ul>
 		</div> 
 		
@@ -96,7 +96,10 @@
 			<h3>Events</h3>
 			<ul>
 				<li>
-					<a href="rename.php">Mylar is reborn.  Long live Mylyn!</a>
+					<font size=+1>Please note that the project rename is still underway.  See the <a href="rename.php">Rename FAQ</a> for details.</font>
+				</li>
+				<li>
+					Mik's blog: <a href="http://tasktop.com/blog/?p=6">Mylar is reborn.  Long live Mylyn!</a>
 				</li>
 			
 				<!--

e3a1c24fab1f12fe2e8a9cbe634c28a4a6fc8dd6
mkersten <mkersten>
2007-06-27 23:10:41 +0000

    Incomplete - task 103879: manage eclipse.org/mylyn web site

--- index.php
+++ index.php
@@ -20,30 +20,7 @@ $html = <<<EOHTML
 					It does this by making tasks a first class part of Eclipse, and integrating rich and offline editing for
 					repositories such as Bugzilla, Trac, and JIRA.  Once your tasks are integrated, Mylyn monitors your work activity to identify information relevant to the task-at-hand, and uses this task context to focus the Eclipse UI on the interesting information, hide the uninteresting, and automatically find what's related. This puts the information you need to get work done at your fingertips and improves productivity by reducing searching, scrolling, and navigation. By making task context explicit Mylyn also facilitates multitasking, planning, reusing past efforts, and sharing expertise.
 					</p>
-					<table border="0" cellpadding="10">
-						<tr>
-							<td width=20></td>
-		          			<td><a href="/mylyn/downloads"><img border=0 src="images/downloads.gif">Downloads: get the latest and greatest</a></td>
-		          		</tr> 	 
-			        	<tr>
-			        		<td width=20></td>
-		          			<td><a href="/mylyn/community"><img border=0 src="images/community.jpg">Community: get help and interact with users and developers</a></td>
-		          		</tr>
-		         	</table>
-					<div class="homeitem3col">
-						<h3>News</h3>
-						<ul>
-							<li>
-								Mylyn 2.0 will be part of the default Eclipse downloads as of June 29th.
-							</li>
-							<li>
-								FAQ: <a href="rename.php">Project renamed from Mylar to Mylyn</a>, Blog: <a href="http://tasktop.com/blog/?p=6">Mylar is reborn. Long live Mylyn!</a>
-							</li>
-							<li>
-								...
-							</li>
-						 </ul>	
-					</div> 
+					
 		  		</td>
           	</tr>
         </table>

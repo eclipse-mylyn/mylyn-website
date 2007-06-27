7f1f28793850159d9335c8feaaeca4adab830ad5
mkersten <mkersten>
2007-06-27 23:19:07 +0000

    Incomplete - task 103879: manage eclipse.org/mylyn web site

--- index.php
+++ index.php
@@ -13,11 +13,21 @@ $html = <<<EOHTML
       		<td valign="top">
 	          	<a href="images/mylyn-2.0.png"><img border=0 src="images/tasklist-splash.jpg"></a>
       		</td>
+      		<td>
+      			xxx
+      		</td>
+      	</tr>
+      	<!--
         	<td valign="top">
         		<p>
 				Mylyn is a Task-Focused UI for Eclipse that reduces information overload and makes multi-tasking easy.  
 				It does this by making tasks a first class part of Eclipse, and integrating rich and offline editing for
-				repositories such as Bugzilla, Trac, and JIRA.  Once your tasks are integrated, Mylyn monitors your work activity to identify information relevant to the task-at-hand, and uses this task context to focus the Eclipse UI on the interesting information, hide the uninteresting, and automatically find what's related. This puts the information you need to get work done at your fingertips and improves productivity by reducing searching, scrolling, and navigation. By making task context explicit Mylyn also facilitates multitasking, planning, reusing past efforts, and sharing expertise.
+				repositories such as Bugzilla, Trac, and JIRA.  Once your tasks are integrated, Mylyn monitors your work 
+				activity to identify information relevant to the task-at-hand, and uses this task context to focus the 
+				Eclipse UI on the interesting information, hide the uninteresting, and automatically find what's related. 
+				This puts the information you need to get work done at your fingertips and improves productivity by reducing 
+				searching, scrolling, and navigation. By making task context explicit Mylyn also facilitates multitasking, 
+				planning, reusing past efforts, and sharing expertise.
 				</p>
 				<table border="0" cellpadding="10">
 					<tr>
@@ -45,6 +55,7 @@ $html = <<<EOHTML
 				</div> 
 	  		</td>
       	</tr>
+      	-->
     </table>
 </div>			
 

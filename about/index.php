d181bb76b795f6c8087c861dbbc660575f4d9580
mkersten <mkersten>
2007-07-09 22:00:25 +0000

    NEW - bug 194875: manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- about/index.php
+++ about/index.php
@@ -30,7 +30,7 @@
 					The <a href="http://en.wikipedia.org/wiki/Myelin">Myelin substance</a> accelerates our 
 					thoughts by making neurons much more efficient at conducting electricity. The Mylyn name 
 					is a tribute to this substance, as many users claim that our tools increase efficiency 
-					to the point where we can <b>code at the speed of thought</b>. 
+					to the point where we can <i>code at the speed of thought</i>. 
 					The preferred pronunciation is 'mIl&n, similar to the pronunciation of "myelin".  The
 					project was <a href="http://www.eclipse.org/mylyn/rename.php">previously called Mylar</a>.
 				</li>

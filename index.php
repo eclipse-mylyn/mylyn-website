b37ec5a876eaa4b07d18971d8b7d5d1e74f7a21d
mkersten <mkersten>
2007-07-17 19:28:09 +0000

    NEW - bug 194875: manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- index.php
+++ index.php
@@ -88,10 +88,7 @@ $html = <<<EOHTML
 					<option>3</option>
 					<option>2</option>
 					<option>1</option>
-					</select><input type="hidden" name="ratinglid" value="560">
-					<input type="hidden" name="ratinguser" value="outside">
-					<input type="hidden" name="req" value="addrating">
-					<input type="submit" value="Vote">
+					</select><input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote">
 					</form>
 				</font>
 	  		</td>

be4319688556fecbf915f4117cd507af444d4f01
mkersten <mkersten>
2007-07-17 19:26:35 +0000

    NEW - bug 194875: manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- index.php
+++ index.php
@@ -75,8 +75,7 @@ $html = <<<EOHTML
 				</div> 
 				
 				<form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
-					<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">Comment on Mylyn at EPIC</a>
-					<select name="rating">
+					<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">Rate and comment on Mylyn at EPIC</a><select name="rating">
 					<option selected>10</option>
 					<option>9</option>
 					<option>8</option>

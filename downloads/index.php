59b83937ae2d5520cca2a55fe861d54be0c1e90a
mkersten <mkersten>
2007-08-28 18:55:12 +0000

    REOPENED - bug 195295: fix layout of download page on IE6 https://bugs.eclipse.org/bugs/show_bug.cgi?id=195295

--- downloads/index.php
+++ downloads/index.php
@@ -42,7 +42,7 @@ $html = <<<EOHTML
     			</div>
 	</div>	
 
-    <div class="homeitem3col" style="width:95%">
+    <div class="homeitem3col" style="margin-left:50px; width:93%">
 		<h3>Mylyn 2.1 Milestone Update Sites</h3> 
 		<ul>
 			<li>
@@ -58,7 +58,7 @@ $html = <<<EOHTML
 		</ul>	
 	</div>
 	
-    <div class="homeitem3col" style="margin-left:20px; width:93%">
+    <div class="homeitem3col" style="margin-left:50px; width:93%">
 		<h3>Mylyn 2.0 Update Sites</h3> 
 		<br>&nbsp;&nbsp;&nbsp;Requires: <a href="http://java.sun.com/j2se/1.5.0/download.jsp">JRE 5.0</a>.  Supports: <a href="http://www.bugzilla.org/">Bugzilla</a> 2.18 and later, <a href="http://trac.edgewall.org/">Trac</a> 0.9 and later				
 		<ul>
@@ -83,7 +83,7 @@ $html = <<<EOHTML
 		</ul>	
 	</div>
 
-	<div class="homeitem3col" style="width:95%">
+	<div class="homeitem3col" style="margin-left:50px; width:93%">
 		<h3>Mylyn 2.0 Extensions</h3>
 		<br>&nbsp;&nbsp;&nbsp;For additional features use the following update sites. 
 		<ul>

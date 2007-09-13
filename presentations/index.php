9a9fba9ba3a7f5c66369fdfbcbd5df0dfb38bf96
mkersten <mkersten>
2007-09-13 19:08:18 +0000

    ASSIGNED - bug 194875: manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- presentations/index.php
+++ presentations/index.php
@@ -39,9 +39,8 @@
 			<h3>Selected Presentations</h3>
 			<ul>
 				<li>
-					<a href="http://live.eclipse.org/node/314"><img border=0 src="/mylyn/images/icon-presentation.gif"></a> 
-					<a href="/mylyn/presentations/2007-07-eclipse-mylyn-webinar.ppt"><img border=0 src="/mylyn/images/icon-ppt.gif"></a>
-                    <a href="http://live.eclipse.org/node/314">Eclipse 3.3 and Mylyn 2.0: Taking the Java IDE to the Next Level</a> by Mik Kersten (July 2007, 55 min)
+					<a href="/mylyn/presentations/2007-07-eclipse-mylyn-webinar.ppt"><img border=0 src="/mylyn/images/icon-ppt.gif">
+                    Eclipse 3.3 and Mylyn 2.0: Taking the Java IDE to the Next Level</a> by Mik Kersten (July 2007, 55 min)
                 </li> 
 				<li>
 					<a href="http://live.eclipse.org/node/325"><img border=0 src="/mylyn/images/icon-presentation.gif">

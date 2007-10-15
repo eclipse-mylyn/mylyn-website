a904b58384bae6719a3a7f1919a1d1aea6e69548
mkersten <mkersten>
2007-10-15 18:59:29 +0000

    ASSIGNED - bug 194875: manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- community/index.php
+++ community/index.php
@@ -23,18 +23,6 @@
 <div id="maincontent">
 	<div id="midcolumn">
 		<!--	<div align="center"><h1>$pageTitle</h1></div> -->
-		
-        <table border="0"> 
-        	<tr>
-        		<td valign="top">
-        			<a href="/mylyn/bugs"><img border="0" src="/mylyn/images/bug-report.png"></a>
-        		</td>
-		        <td>
-			        <p>The preferred way of reporting and discussing defects and enhancement requests is via <a href="/mylyn/bugs">Bug Reports</a>.
-			    </td> 
-			</tr>
-		</table>
-         
 		<div class="homeitem3col">
 			<h3>Newsgroup</h3>
 			<ul>
@@ -75,6 +63,20 @@
 				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=%5Bdiscussion%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
 					Bugzilla discussions</a> of ideas, enhancements, and UI issues.</li>
 			</ul>
+		</div>
+		
+		<div class="homeitem3col">
+			<h3>Bugzilla</h3>
+			<table border="0"> 
+	        	<tr>
+	        		<td valign="top">
+	        			<a href="/mylyn/bugs"><img border="0" src="/mylyn/images/bug-report.png"></a>
+	        		</td>
+			        <td>
+				        <p>The preferred way of reporting and discussing defects and enhancement requests is via <a href="/mylyn/bugs">Bug Reports</a>.
+				    </td> 
+				</tr>
+			</table>
 		</div>
 	<p>&nbsp;</p>
 	<p>&nbsp;</p>

6dd944492ef4b29786cd1748bd3ccae1fece6e46
mkersten <mkersten>
2007-10-15 18:39:57 +0000

    ASSIGNED - bug 194875: manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- community/index.php
+++ community/index.php
@@ -27,7 +27,7 @@
         <table border="0"> 
         	<tr>
         		<td valign="top">
-        			<a href="/mylyn/bugs"><img src="/mylyn/images/bug-report.png"></a>
+        			<a href="/mylyn/bugs"><img border="0" src="/mylyn/images/bug-report.png"></a>
         		</td>
 		        <td>
 			        <p>The preferred way of reporting and discussing defects and enhancement requests is via <a href="/mylyn/bugs">Bug Reports</a>.
@@ -37,7 +37,7 @@
 		</table>
          
 		<div class="homeitem3col">
-			<h3>Newsgroup and Mailing Lists</h3>
+			<h3>Newsgroup</h3>
 			<ul>
 				<li>
 	                <a href="news://news.eclipse.org/eclipse.tools.mylyn"> 
@@ -62,6 +62,30 @@
 					Bugzilla discussions</a> of ideas, enhancements, and UI issues.</li>
 			</ul>
 		</div>
+
+		<div class="homeitem3col">
+			<h3>Mailing Lists</h3>
+			<img border="0" src="/mylyn/images/bug-report.png">
+			<ul> 
+				<li>
+	                <a href="http://dev.eclipse.org/mailman/listinfo/mylyn-announce">
+					mylyn-announce@eclipse.org:</a> major releases and events
+					<a href="http://dev.eclipse.org/mhonarc/lists/mylyn-announce/maillist.html">[archive]</a>
+					<a href="http://dev.eclipse.org/mhonarc/lists/mylar-announce/maillist.html">[old archive]</a></li>
+				<li>
+	                <a href="http://dev.eclipse.org/mailman/listinfo/mylyn-integrators">
+					mylyn-integrators@eclipse.org:</a> developer discussions and planning
+					<a href="http://dev.eclipse.org/mhonarc/lists/mylyn-integrators/maillist.html">[archive]</a></li>
+				<li>
+	                <a href="http://dev.eclipse.org/mailman/listinfo/mylyn-dev">
+					mylyn-dev@eclipse.org:</a> developer discussions and planning
+					<a href="http://dev.eclipse.org/mhonarc/lists/mylyn-dev/maillist.html">[archive]
+					<a href="http://dev.eclipse.org/mhonarc/lists/mylar-dev/maillist.html">[old archive]</a></li>
+				<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=%5Bdiscussion%5D&product=Mylar&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
+					Bugzilla discussions</a> of ideas, enhancements, and UI issues.</li>
+			</ul>
+		</div>
+
 		<div class="homeitem3col">
 			<h3>Contribute</h3>
 			<ul>

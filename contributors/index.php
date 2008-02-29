8d1c70af918f7277712676b23949c53a60dd9b6a
spingel <spingel>
2008-02-29 03:01:32 +0000

    Version update

--- contributors/index.php
+++ contributors/index.php
@@ -133,7 +133,7 @@
 					[11 bugs]</a>
 				</li>
 				<li> 
-					<a href="http://code.google.com/soc/2007/"><img border=0 src="/mylyn/images/sites/google.png"></a> Balazs Brinkus, <a href="http://code.google.com/soc/2007/">Google Summer of Code<a>
+					<a href="http://code.google.com/soc/2007/"><img border=0 src="/mylyn/images/sites/google.png"></a> Balazs Brinkus, <a href="http://code.google.com/soc/2007/">Google Summer of Code</a>
 					<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylyn&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailassigned_to1=1&emailtype1=substring&email1=balazs.brinkus%40gmail.com&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
 					[7 bugs]</a>
 				</li>	

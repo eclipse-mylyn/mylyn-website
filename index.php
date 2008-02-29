79a58b121b01130edcf9683fe8f9d52529d6a15c
spingel <spingel>
2008-02-29 22:53:53 +0000

    Version update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $pageAuthor		= "Mik Kersten";
 $html = <<<EOHTML
 
 <div id="maincontent">
-<div id="midcolumn" style="width:93%">
+<div id="midcolumn">
 	<table border="0" cellpadding="5">
      	<tr> 
       		<td valign="top">

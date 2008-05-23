4e370f52c4da49bb0e4c1d9ab1ede3219248b0a0
mkersten <mkersten>
2008-05-23 06:36:06 +0000

    NEW - bug 227660: track Mylyn 3.0 API creation and migration https://bugs.eclipse.org/bugs/show_bug.cgi?id=227660

--- downloads/index.php
+++ downloads/index.php
@@ -95,8 +95,8 @@ $html = <<<EOHTML
 
 
     <div class="homeitem3col" style="margin-left:40px; width:90%">
-		<h3>3.0 RCs&nbsp;<a title="More Info" href="http://wiki.eclipse.org/index.php/Mylar_User_Guide#Installation"><img border=0 src="/mylyn/images/icon-help.gif"></a></h3>
-		<br>&nbsp;&nbsp;&nbsp;<a href="http://wiki.eclipse.org/Mylyn/3.0_Plan#Schedule">Release Schedule</a>
+		<h3>3.0 RC Builds&nbsp;<a title="More Info" href="http://wiki.eclipse.org/index.php/Mylar_User_Guide#Installation"><img border=0 src="/mylyn/images/icon-help.gif"></a></h3>
+		<br>&nbsp;&nbsp;&nbsp;<a href="http://wiki.eclipse.org/Mylyn/3.0_Plan#Schedule">Release Schedule and plan</a>
 		<ul>
 			<li>
 				<b>download.eclipse.org/tools/mylyn/update/dev/e3.4</b>&nbsp;&nbsp;(Eclipse 3.4RC1)

130395bfdeff316b985ee379e57f6a33c25a176c
mkersten <mkersten>
2008-06-26 02:08:33 +0000

    ASSIGNED - bug 194875: manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- downloads/index.php
+++ downloads/index.php
@@ -57,9 +57,7 @@ $html = <<<EOHTML
 					</form>
 					</font>
 		        	<b>New users</b>: you can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
-			        then look updates using the sites below.
-			        </p>
-			        <b>Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
+			        then look updates using the sites below.  Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
 			        </p>
     			</div>
 	</div>		

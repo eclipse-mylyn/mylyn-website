f03f9b3d580d579de349806f1e4b14f4a8e87cc7
spingel <spingel>
2008-07-07 20:14:48 +0000

    RESOLVED - bug 238751: TRAC closed (fixed) tasks are not displayed as complete (green) by Mylyn https://bugs.eclipse.org/bugs/show_bug.cgi?id=238751

--- downloads/index.php
+++ downloads/index.php
@@ -56,9 +56,13 @@ $html = <<<EOHTML
 					</select>&nbsp;<input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote">
 					</form>
 					</font>
-		        	<b>New users</b>: you can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
+		        	
+		        	<p><b>New users</b>: you can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
 			        then look updates using the sites below.  <b>Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
 			        </p> 
+			        
+			        <p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation
+	Software User Agreement</a> unless otherwise specified.</p>
     			</div>
 	</div>		
 
@@ -191,9 +195,6 @@ $html = <<<EOHTML
 		</table>
 		<br>&nbsp;
 	</div>
-	
-	<p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation
-	Software User Agreement</a> unless otherwise specified.</p>
 </div>
 
 EOHTML;

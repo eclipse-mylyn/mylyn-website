ec1c5e9a585c5b3bdb65831cb4a8caad53ebe0cb
mkersten <mkersten>
2008-08-04 21:44:31 +0000

    ASSIGNED - bug 194875: manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- start/index.php
+++ start/index.php
@@ -37,7 +37,7 @@
 						 		<font size="+1"><a href="http://live.eclipse.org/node/573">
 						 			<a href="http://live.eclipse.org/node/573">
 						 				Mylyn 3.0: Code at the Speed of Thought</a></font>
-						 			<br>by Mik Kersten (Tasktop Technologies) webcast on Eclipse Live <i>July 30, 2008</i>
+						 			<br>by Mik Kersten (Tasktop Technologies) webcast on Eclipse Live <i>[August 2008]</i>
 						 			<br>Provides a 45 minute overview of the tool and technology, along with demos of the key features.
 						 		</font>
 						 	</td>
@@ -46,11 +46,11 @@
 				</li>
 				<li>
 		            <a href="http://www.ibm.com/developerworks/java/library/j-mylyn1/?ca=dgr-eclipse-1"><img border=0 src="/mylyn/images/icon-webdoc.gif">
-		           	Mylyn 2.0, Part1: Integrated task management</a> by Mik Kersten (Aug 2007)
+		           	Mylyn 2.0, Part1: Integrated task management</a> by Mik Kersten <i>[August 2007<i/>
 	            </li>
 	            <li> 
 		            <a href="http://www.ibm.com/developerworks/java/library/j-mylyn2/?ca=dgr-eclipse-1"><img border=0 src="/mylyn/images/icon-webdoc.gif">
-		           	Mylyn 2.0, Part 2: Automated context management</a> by Mik Kersten (Aug 2007)
+		           	Mylyn 2.0, Part 2: Automated context management</a> by Mik Kersten <i>[August 2007</i>
 	            </li>
 	            <!--
 	            <li>
@@ -75,7 +75,8 @@
 			</ul>
 		</div>     
 
-		<p>		
+		<p>	
+		<br>&nbsp;	
 		For questions about using Mylyn see the <a href="/mylyn/community">Community Page</a>.
         </p>
 		

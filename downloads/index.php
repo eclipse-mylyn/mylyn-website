c72f8dea02c4c8a2a778d255f6b028cd917795c4
relves <relves>
2008-12-05 19:18:10 +0000

    NEW - bug 255291: [website] improve download and community pages and streamline site https://bugs.eclipse.org/bugs/show_bug.cgi?id=255291

--- downloads/index.php
+++ downloads/index.php
@@ -180,40 +180,42 @@ $html = <<<EOHTML
 			</table>
 		</div>
 		
+		
+		<div id="rightcolumn" style="padding-right: 20px">
+			<div class="sideitem">
+					<h6>New Users</h6>
+				<p style="padding-left: 5px;">You can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
+					        then look for updates using the sites below.  <br><b>Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
+					        </p> 
+			</div>
+			<div class="sideitem">
+					<h6>License</h6>
+				<p style="padding-left: 5px;">All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation
+			Software User Agreement</a> unless otherwise specified.</p>
+			</div>
+			<div class="sideitem">
+			<h6><img border=0 src="/mylyn/images/icon-presentation.gif"> Support Mylyn</h6>
+			 <p style="padding-left: 5px; text-align: center;"><form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
+				<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">
+				Vote on EPIC</a><br>
+				<select name="rating">
+				<option selected>10</option>
+				<option>9</option>
+				<option>8</option>
+				<option>7</option>
+				<option>6</option>
+				<option>5</option>
+				<option>4</option>
+				<option>3</option>
+				<option>2</option>
+				<option>1</option>
+				</select>&nbsp;<input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote">
+				</form></p>
+			</div>
+		</div>
+		
 	</div>	
 
-	<div id="rightcolumn" style="padding-right: 20px">
-		<div class="sideitem">
-				<h6>New Users</h6>
-			<p style="padding-left: 5px;">You can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
-				        then look for updates using the sites below.  <br><b>Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
-				        </p> 
-		</div>
-		<div class="sideitem">
-				<h6>License</h6>
-			<p style="padding-left: 5px;">All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation
-		Software User Agreement</a> unless otherwise specified.</p>
-		</div>
-		<div class="sideitem">
-		<h6><img border=0 src="/mylyn/images/icon-presentation.gif"> Support Mylyn</h6>
-		 <p style="padding-left: 5px; text-align: center;"><form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
-			<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">
-			Vote on EPIC</a><br>
-			<select name="rating">
-			<option selected>10</option>
-			<option>9</option>
-			<option>8</option>
-			<option>7</option>
-			<option>6</option>
-			<option>5</option>
-			<option>4</option>
-			<option>3</option>
-			<option>2</option>
-			<option>1</option>
-			</select>&nbsp;<input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote">
-			</form></p>
-		</div>
-	</div>
 	<!--<div align="homeitem3col" style="width:85%">&nbsp;</div>--> 
 	
 EOHTML;

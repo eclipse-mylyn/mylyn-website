1941a99bad63cac08e6b8d0a5e5a7ffa004980ca
relves <relves>
2008-12-08 20:22:02 +0000

    NEW - Task 254: update Mylyn site and wiki pages https://tasktop.com/dev/ops/show_bug.cgi?id=254

--- downloads/index.php
+++ downloads/index.php
@@ -44,37 +44,37 @@ $html = <<<EOHTML
 			        <p>
 					</font>
     			</div>
-	</div>
 	
-		<div>
-				<h6>New Users</h6>
-			<p style="padding-left: 5px;">You can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
-				        then look for updates using the sites below.  <br><b>Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
-				        </p> 
-		</div>
-		<div>
-				<h6>License</h6>
-			<p style="padding-left: 5px;">All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation
-		Software User Agreement</a> unless otherwise specified.</p>
-		</div>
-		<div>
-		<h6><img border=0 src="/mylyn/images/icon-presentation.gif"> Support Mylyn</h6>
-		 <p style="padding-left: 5px; text-align: center;"><form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
-			<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">
-			Vote on EPIC</a><br>
-			<select name="rating">
-			<option selected>10</option>
-			<option>9</option>
-			<option>8</option>
-			<option>7</option>
-			<option>6</option>
-			<option>5</option>
-			<option>4</option>
-			<option>3</option>
-			<option>2</option>
-			<option>1</option>
-			</select>&nbsp;<input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote">
-			</form></p>
+			<div>
+					<h6>New Users</h6>
+				<p style="padding-left: 5px;">You can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
+					        then look for updates using the sites below.  <br><b>Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
+					        </p> 
+			</div>
+			<div>
+					<h6>License</h6>
+				<p style="padding-left: 5px;">All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation
+			Software User Agreement</a> unless otherwise specified.</p>
+			</div>
+			<div>
+			<h6><img border=0 src="/mylyn/images/icon-presentation.gif"> Support Mylyn</h6>
+			 <p style="padding-left: 5px; text-align: center;"><form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
+				<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">
+				Vote on EPIC</a><br>
+				<select name="rating">
+				<option selected>10</option>
+				<option>9</option>
+				<option>8</option>
+				<option>7</option>
+				<option>6</option>
+				<option>5</option>
+				<option>4</option>
+				<option>3</option>
+				<option>2</option>
+				<option>1</option>
+				</select>&nbsp;<input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote">
+				</form></p>
+			</div>
 		</div>		
 
 	<div style="border:none; margin-left:40px; width:75%;">

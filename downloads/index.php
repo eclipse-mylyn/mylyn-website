d63e07851758a930ec3c8c439e2422dc5c656bde
relves <relves>
2008-12-08 21:38:59 +0000

    NEW - bug 255291: [website] improve download and community pages and streamline site https://bugs.eclipse.org/bugs/show_bug.cgi?id=255291

--- downloads/index.php
+++ downloads/index.php
@@ -42,20 +42,7 @@ $html = <<<EOHTML
 					<a href="http://wiki.eclipse.org/index.php/Mylyn_User_Guide#Installation">Update Manager (instructions available)</a>.
 			        </p>
 			        
-			        <div style="clear: right;">
-			        <h4>New Users</h4>
-					<p style="padding-left: 5px;">You can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
-					        then look for updates using the sites below.  <br><b>Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
-					        </p>
-					        </div>
-					        
-						<div style="clear: right;"><h4>License</h4>
-							<p style="padding-left: 5px;">All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation
-					Software User Agreement</a> unless otherwise specified.</p>
-					</div>
-			        
-			        
-				        <div class="sideitem" style="clear: right; float: right;">
+			        <div class="sideitem" style="clear: right; float: right;">
 					<h6><img border=0 src="/mylyn/images/icon-presentation.gif"> Support Mylyn</h6>
 					 <p style="padding-left: 5px; text-align: center;"><form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
 						<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">
@@ -75,6 +62,24 @@ $html = <<<EOHTML
 						</form></p>
 					</div>
     			</div>
+			        
+			        
+			        <table>
+			        <tr><td>
+			        <h4>New Users</h4>
+					<p style="padding-left: 5px;">You can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
+					        then look for updates using the sites below.  <br><b>Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
+					        </p>
+					  </td>
+					  <td>
+					  	<h4>License</h4>
+							<p style="padding-left: 5px;">All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation
+					Software User Agreement</a> unless otherwise specified.</p>
+					</td>
+					</tr>
+					</table>
+			        
+			        
     			
 		</div>		
 

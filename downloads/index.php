d88742023539c1d44baafb0b86a56c4608a20bc8
relves <relves>
2008-12-08 21:32:00 +0000

    NEW - bug 255291: [website] improve download and community pages and streamline site https://bugs.eclipse.org/bugs/show_bug.cgi?id=255291

--- downloads/index.php
+++ downloads/index.php
@@ -61,15 +61,16 @@ $html = <<<EOHTML
 						</select>&nbsp;<input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote">
 						</form></p>
 					</div>
-			        
+			        <div style="float: right;">
 			        <h4>New Users</h4>
 					<p style="padding-left: 5px;">You can download Eclipse with Mylyn in most of the <a href="http://www.eclipse.org/downloads/">Eclipse Bundles</a>, 
 					        then look for updates using the sites below.  <br><b>Eclipse 3.2 users</b>: use the <a href="http://www.eclipse.org/mylyn/builds">Mylyn 2.0 release</a>.
 					        </p>
-					        
-					        <h4>License</h4>
+					        </div>
+					        <div style="float: right;"><h4>License</h4>
 					<p style="padding-left: 5px;">All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation
 			Software User Agreement</a> unless otherwise specified.</p>
+			</div>
     			</div>
     			
 		</div>		

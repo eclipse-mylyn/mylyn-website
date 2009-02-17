f5ef3f956613e6e2d744d07c92f8d89c5f7d6dc5
relves <relves>
2009-02-17 18:49:43 +0000

    wjax presentation

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ $html = <<<EOHTML
 									<td width="10>"</td>
 									<td>
 								 		<font size="+1"><a href="http://live.eclipse.org/node/676">
-								 				Less is More: Redefining the ÒIÓ of the IDE</a></font>
+								 				Less is More: Redefining the "I" of the IDE</a></font>
 								 			<br>by Mik Kersten (Tasktop Technologies) webcast available on Eclipse Live <i>February 17, 2009</i>
 								 		</font>
 								 	</td>

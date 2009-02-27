3f1df0600f7d7afe3dfdc552912062e7865027be
mkersten <mkersten>
2009-02-27 21:33:05 +0000

    ASSIGNED - bug 194875: manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ $html = <<<EOHTML
 									<td>
 								 		<font size="+1"><a href="http://live.eclipse.org/node/676">
 								 				Less is More: Redefining the "I" of the IDE</a></font>
-								 			<br>by Mik Kersten (Tasktop), W-JAX keynote discussing Mylyn and the task-focused interface<i>February 17, 2009</i>
+								 			<br>by Mik Kersten (Tasktop), W-JAX keynote discussing Mylyn and the task-focused interface, <i>February 17, 2009</i>
 								 		</font>
 								 	</td>
 								 </tr>

9d54ace7329eaef6ae3f94a51f502033b4f2dbbf
mkersten <mkersten>
2009-03-17 23:08:08 +0000

    NEW - bug 265978: review web site content and update site links to non-Eclipse.org content https://bugs.eclipse.org/bugs/show_bug.cgi?id=265978

--- downloads/index.php
+++ downloads/index.php
@@ -27,38 +27,6 @@
 		
 $html = <<<EOHTML
 
-	<div id="rightcolumn">
-		<div class="sideitem">
-			<h6>Support Mylyn</h6>
-			 <div style="margin-top:10px; margin-left:10px;">
-			 <form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
-				<!--
-				<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">
-				Vote on EPIC: </a>&nbsp; -->
-				<select name="rating">
-				<option selected>10</option>
-				<option>9</option>
-				<option>8</option>
-				<option>7</option>
-				<option>6</option>
-				<option>5</option>
-				<option>4</option>
-				<option>3</option>
-				<option>2</option>
-				<option>1</option>
-				</select>&nbsp;<input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote on EPIC">
-				</form>
-		</div>
-		</div>    
-
-		<div class="sideitem">
-			<h6>Announce List</h6>
-			 <ul><li>
-				<a href="http://dev.eclipse.org/mailman/listinfo/mylyn-announce">Get notified</a> of Mylyn releases.
-			  </li></ul>
-		</div>    
-	</div>
-
 	<div style="margin-top:30px; margin-left:40px; width:75%">
     	<p>
         The minimal Mylyn install is a standalone Task List.  Optional Task Repository Connectors integrate the

977bcba29d1d2579e919b694e50e05d53c569796
mkersten <mkersten>
2009-03-17 20:17:44 +0000

    NEW - bug 265978: review web site content and update site links to non-Eclipse.org content https://bugs.eclipse.org/bugs/show_bug.cgi?id=265978

--- downloads/index.php
+++ downloads/index.php
@@ -197,7 +197,7 @@ $html = <<<EOHTML
 		Software User Agreement</a> unless otherwise specified.</p>
 		</div>
 		<div class="sideitem">
-		<h6><img border=0 src="/mylyn/images/icon-presentation.gif"> Support Mylyn</h6>
+		<h6>Support Mylyn</h6>
 		 <p style="padding-left: 5px; text-align: center;"><form method="post" action="http://www.eclipseplugincentral.com/Web_Links.html">
 			<a href="http://www.eclipseplugincentral.com/Web_Links-index-req-viewlink-cid-587.html">
 			Vote on EPIC</a><br>
@@ -213,7 +213,7 @@ $html = <<<EOHTML
 			<option>2</option>
 			<option>1</option>
 			</select>&nbsp;<input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote">
-			</form></p>
+			</form><br>&nbsp;</p>
 		</div>
 	</div>
 	-->

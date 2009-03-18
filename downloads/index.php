5d405938def969e19bafde13fc3e4e9176122f98
mkersten <mkersten>
2009-03-18 18:33:17 +0000

    NEW - bug 265978: review web site content and update site links to non-Eclipse.org content https://bugs.eclipse.org/bugs/show_bug.cgi?id=265978

--- downloads/index.php
+++ downloads/index.php
@@ -44,7 +44,7 @@ $html = <<<EOHTML
 				<option>3</option>
 				<option>2</option>
 				<option>1</option>
-				</select>&nbsp;<input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote on EPIC">
+				</select> <input type="hidden" name="ratinglid" value="560"><input type="hidden" name="ratinguser" value="outside"><input type="hidden" name="req" value="addrating"><input type="submit" value="Vote on EPIC">
 				</form>
 				</div>
 		</div>    

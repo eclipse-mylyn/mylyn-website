0531abce6a4ed22285557d31c32ee5638484da9d
mkersten <mkersten>
2009-03-18 23:37:14 +0000

    RESOLVED - bug 265978: review web site content and update site links to non-Eclipse.org content https://bugs.eclipse.org/bugs/show_bug.cgi?id=265978

--- index.php
+++ index.php
@@ -19,9 +19,9 @@ $html = <<<EOHTML
 				Mylyn is a task-focused interface for Eclipse that reduces information overload and makes multi-tasking easy.  
 				It does this by making tasks a first class part of Eclipse, and integrating rich and offline editing for
 				repositories such as Bugzilla, Trac, and JIRA.  Once your tasks are integrated, Mylyn monitors your work 
-				activity to identify information relevant to the task-at-hand, and uses this task context to focus the 
-				Eclipse UI on the interesting information, hide the uninteresting, and automatically find what's related. 
-				This puts the information you need to get work done at your fingertips and improves productivity by reducing 
+				activity to identify relevant information, and uses this task context to focus the 
+				user interface on the task-at-hand.
+				This puts the information you need at your fingertips and improves productivity by reducing 
 				searching, scrolling, and navigation. By making task context explicit Mylyn also facilitates multitasking, 
 				planning, reusing past efforts, and sharing expertise.
 

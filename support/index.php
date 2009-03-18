6aad15e73caadbed00fa71fa26f28fd8bf640622
mkersten <mkersten>
2009-03-18 17:54:33 +0000

    NEW - bug 265978: review web site content and update site links to non-Eclipse.org content https://bugs.eclipse.org/bugs/show_bug.cgi?id=265978

--- support/index.php
+++ support/index.php
@@ -75,11 +75,11 @@
 				
 				<li>
 				  <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr&short_desc=%5Bconnector%5D&product=Mylyn&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-				  <img border=0 src="/mylyn/images/icon-presentation.gif"> Vote for Mylyn Connectors</a> to your task, bug, issue and source repositories
+				  <img border=0 src="/mylyn/images/icon-presentation.gif">&nbsp;&nbsp;Vote for Mylyn Connectors</a> to your task, bug, issue and source repositories
 				</li>
 				<li>
 				  <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr&short_desc=%5Bbridge%5D&product=Mylyn&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
-				  <img border=0 src="/mylyn/images/icon-presentation.gif"> Vote for Mylyn Bridges</a> for your programming language or framework
+				  <img border=0 src="/mylyn/images/icon-presentation.gif">&nbsp;&nbsp;Vote for Mylyn Bridges</a> for your programming language or framework
 				</li>
 			</ul> 
 				

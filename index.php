fd52e5d271324e321c61e47a07effe8624d7b9c3
relves <relves>
2009-03-24 00:43:40 +0000

    tutorial

--- index.php
+++ index.php
@@ -95,6 +95,10 @@ $html = <<<EOHTML
 				<div class="homeitem3col">
 					<h3>News</h3>
 					<ul>
+	            		<li>
+		            		<a href="presentations/2009-03-mylyn-tutorial.pdf">
+		           			<img src="images/star.png"/>&nbsp;&nbsp; EclipseCon Slides (pdf)</a> <i>March 23th, 2009</i>
+	            		</li>
 						<li>
 		            		<a href="http://www.eweek.com/c/s/Application-Development/">
 		           			<img src="images/star.png"/>&nbsp;&nbsp; eWeek: Tasktop Upgrades Mylyn, Tasktop Offerings</a> <i>March 20th, 2009</i>
@@ -102,10 +106,6 @@ $html = <<<EOHTML
 						<li>
 		            		<a href="http://tasktop.com/blog/eclipse/tasktop-14-and-eclipse-mylyn-31-released">
 		           			<img src="images/star.png"/>&nbsp;&nbsp; Mylyn 3.1 released!</a> <i>March 17th, 2009</i>
-	            		</li>
-	            		<li>
-		            		<a href="presentations/2009-03-mylyn-tutorial.pdf">
-		           			<img src="images/star.png"/>&nbsp;&nbsp; EclipseCon Slides (pdf)</a>
 	            		</li>
 						<li>
 							<table cellspacing="0" cellpadding="0">

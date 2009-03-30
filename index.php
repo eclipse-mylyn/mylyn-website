4859e2e146d6f2c1ea6a6590f4f09b9abb12032a
relves <relves>
2009-03-30 20:39:05 +0000

    NEW - bug 265978: review web site content and update site links to non-Eclipse.org content https://bugs.eclipse.org/bugs/show_bug.cgi?id=265978

--- index.php
+++ index.php
@@ -97,12 +97,8 @@ $html = <<<EOHTML
 					<ul>
 	            		<li>
 	            			<a href="http://www.eclipse.org/downloads/download.php?file=/tools/mylyn/docs/2009-03-mylyn-connector-crash-course-talk.pdf">
-		           			<img src="images/star.png"/>&nbsp;&nbsp; EclipseCon Connector Crash Course Slides (pdf)</a> <i>March 26th, 2009</i><br>
+		           			&nbsp;&nbsp; EclipseCon Connector Crash Course Slides (pdf)</a> <i>March 26th, 2009</i><br>
 		           			<a href="http://www.eclipse.org/downloads/download.php?file=/tools/mylyn/docs/workspace-connector-tutorial.zip">Workspace (zip)</a> or <a href="doc/dev/mylyn-connector-tutorial.psf">Team Project Set (psf)</a>
-	            		</li>
-	            		<li>
-		            		<a href="presentations/2009-03-mylyn-tutorial.pdf">
-		           			<img src="images/star.png"/>&nbsp;&nbsp; EclipesCon Mylyn Tutorial Slides (pdf)</a> <i>March 23th, 2009</i>
 	            		</li>
 						<li>
 		            		<a href="http://www.eweek.com/c/s/Application-Development/">

0c92e49c2b413707f2fe3b61252e9cc70778811f
mkersten <mkersten>
2009-04-15 03:32:36 +0000

    ASSIGNED - bug 194875: [website] manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $html = <<<EOHTML
 
 <div id="maincontent">
 <div id="midcolumn" style="width:98%">
-	<table border="0" cellpadding="5">
+	<table border="0" cellpadding="5" width="100%">
      	<tr> 
         	<td width="600" valign="top">
 				Mylyn is a task-focused interface for Eclipse that reduces information overload and makes multi-tasking easy.  
@@ -174,7 +174,6 @@ $html = <<<EOHTML
 	  		<td valign="top" align="center" width="*">
 	          	<a href="images/mylyn-3.1-screenshot.png"><img alt="Mylyn 2.0 Screenshot" border=0 src="images/tasklist-splash-3.1.png"></a>
       		</td>
-	  		
       	</tr>
     </table>
 </div>

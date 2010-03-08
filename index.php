ae1f8ef43b18757ea82a22bd2e07a1604574bccd
mkersten <mkersten>
2010-03-08 19:26:47 +0000

    ASSIGNED - bug 194875: [website] manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- index.php
+++ index.php
@@ -237,7 +237,8 @@ $html = <<<EOHTML
 	  			<a href="http://marketplace.eclipse.org/content/mylyn">
 						<img src="/mylyn/images/star.png" border="0"/>
 						Star Mylyn as favorite on Eclipse Marketplace</a>
-				<br> 
+				<br>&nbsp;
+				<br>&nbsp;
 	  			<a href="images/mylyn-3.1-screenshot.png"><img alt="Mylyn 3.1 Screenshot" border=0 src="images/tasklist-splash-3.1.png"></a>
 	  			<br>&nbsp;
 	  			<div align="left" id="twtr-search-widget"></div>

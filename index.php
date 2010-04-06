7933b1a8b69a177e24e76daf7edbed100f0b4214
relves <relves>
2010-04-06 18:27:13 +0000

    ASSIGNED - bug 194875: [website] manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- index.php
+++ index.php
@@ -131,7 +131,7 @@ $html = <<<EOHTML
 						<li>
 							<a href="http://event.on24.com/r.htm?e=188674&s=1&k=4690DB5DA995CA264747D73872922FC1&partnerref=tasktop">
 		           			<img src="images/star.png"/>&nbsp;&nbsp;Register</a> for the CollabNet Conference and visit the Tasktop booth to learn more about Mylyn-powered Eclipse integration for TeamForge and ScrumWorks Pro
-							<img align="right" src="images/collabnet-wide.png"/>
+							<img align=RIGHT src="images/collabnet-wide.png"/>
 	            		</li>
 						<li>
 		            		<a href="new/">

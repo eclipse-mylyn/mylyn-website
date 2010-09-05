aa8a95e703462cbcee815c060a5c2242358b733b
fbecker <fbecker>
2010-09-05 07:18:09 +0000

    ASSIGNED - bug 322716: no bugs are shown for David Green https://bugs.eclipse.org/bugs/show_bug.cgi?id=322716

--- contributors/index.php
+++ contributors/index.php
@@ -48,8 +48,8 @@
                 	[1190 bugs]</a>
                 </li>
 				<li> 
-					<a href="http://greensopinion.blogspot.com/"><img border=0 src="/mylyn/images/sites/independent.gif"></a> David Green, <a href="http://greensopinion.blogspot.com/">independent</a>: WikiText
-					<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylyn&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailassigned_to1=1&emailtype1=substring&email1=dgreen99%40gmail.com&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
+					<a href="http://tasktop.com"><img border=0 src="/mylyn/images/sites/tasktop.png"></a> David Green, <a href="http://tasktop.com">Tasktop</a>: WikiText
+					<a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Mylyn&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailassigned_to1=1&emailtype1=substring&email1=David.Green%40tasktop.com&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
 					[304 bugs]</a>
 				</li>
 				<li> 

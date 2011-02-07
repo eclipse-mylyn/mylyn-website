6c795ab32313de2f33d629d6407fcf8c76cc43be
spingel <spingel>
2011-02-07 08:15:30 +0000

    NEW - bug 334968: create websites for sub-projects https://bugs.eclipse.org/bugs/show_bug.cgi?id=334968

--- docs/_projectCommon.php
+++ docs/_projectCommon.php
@@ -1,57 +1,31 @@
 <?php
 
-/*******************************************************************************
- * Copyright (c) 2009 Eclipse Foundation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    
- *******************************************************************************/
-
 	# Set the theme for your project's web pages.
-	# See http://eclipse.org/phoenix/
+	# See the Committer Tools "How Do I" for list of themes
+	# https://dev.eclipse.org/committers/
+	# Optional: defaults to system theme 
 	$theme = "Nova";
 	
 
-	# Define your project-wide Navigation here
-	# This appears on the left of the page if you define a left nav
+	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
+	$Nav->setLinkList(array());
+	$Nav->addNavSeparator("Mylyn", 	"/mylyn/");
+	$Nav->addCustomNav("About", "/projects/project_summary.php?projectid=mylyn.docs", "_self", 3);
+	$Nav->addCustomNav("Plan", "/projects/project-plan.php?projectid=mylyn.docs", "_self", 3);
 	
-	# If you want to override the eclipse.org navigation, uncomment below.
-	# $Nav->setLinkList(array());
-	
-	# Break the navigation into sections
-	$Nav->addNavSeparator("MyProject", 	"/default");
-	$Nav->addCustomNav("Download", "/project/download.php", "_self", 3);
-	$Nav->addCustomNav("Documentation", "/project/documentation.php", "_blank", 3);
-	$Nav->addCustomNav("Support", "/project/support.php", "_blank", 3);
-	$Nav->addCustomNav("Getting Involved", "/project/developers", "_blank", 3);
+	$pageKeywords	= "eclipse, mylyn, docs, wikitext, htmltext, rich editing";
+	$pageAuthor		= "Steffen Pingel";
+	$pageTitle 		= "Mylyn Docs";
 
-	# Define keywords, author and title here, or in each PHP page specifically
-	$pageKeywords	= "xtext, dsl, modeling, domain specific language, textual";
-	$pageAuthor		= "Your name";
-	# $pageTitle 		= "Xtext";
-
-
-	# top navigation bar
-	# To override and replace the navigation with your own, uncomment the line below.
-	# $Menu->setMenuItemList(array());
-	# $Menu->addMenuItem("Home", "/project", "_self");
-	# $Menu->addMenuItem("Download", "/project/download.php", "_self");
-	# $Menu->addMenuItem("Documentation", "/project/documentation.php", "_self");
-	# $Menu->addMenuItem("Support", "/project/support.php", "_self");
-	# $Menu->addMenuItem("Developers", "/project/developers", "_self");
+	$Menu->setMenuItemList(array());
+	$Menu->addMenuItem("Mylyn", "/mylyn/", "_self");
+	$Menu->addMenuItem("About", "/projects/project_summary.php?projectid=mylyn.docs", "_self");
+	$Menu->addMenuItem("Plan", "/projects/project-plan.php?projectid=mylyn.docs", "_self");
 	
-	# To define additional CSS or other pre-body headers
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/default/style.css"/>');
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>' . "\n\t");
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style2.css"/>' . "\n\t");
 	
-	# To enable occasional Eclipse Foundation Promotion banners on your pages (EclipseCon, etc)
 	$App->Promotion = TRUE;
-	
-	# If you have Google Analytics code, use it here
-	# $App->SetGoogleAnalyticsTrackingCode("YOUR_CODE");
 ?>

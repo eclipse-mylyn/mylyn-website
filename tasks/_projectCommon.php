1fb0744fc581c45e599afc2f9adfc0169497a7a7
spingel <spingel>
2011-02-07 06:40:37 +0000

    NEW - bug 334968: create websites for sub-projects https://bugs.eclipse.org/bugs/show_bug.cgi?id=334968

---
+++ tasks/_projectCommon.php
@@ -0,0 +1,31 @@
+<?php
+
+	# Set the theme for your project's web pages.
+	# See the Committer Tools "How Do I" for list of themes
+	# https://dev.eclipse.org/committers/
+	# Optional: defaults to system theme 
+	$theme = "Nova";
+	
+
+	# Define your project-wide Nav bars here.
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# these are optional
+	$Nav->setLinkList(array());
+	$Nav->addNavSeparator("Mylyn", 	"/mylyn/");
+	$Nav->addCustomNav("About", "/projects/project_summary.php?projectid=mylyn.tasks", "_self", 3);
+	$Nav->addCustomNav("Plan", "/projects/project-plan.php?projectid=mylyn.tasks", "_self", 3);
+	
+	$pageKeywords	= "eclipse, mylyn, tasks, bugzilla, trac";
+	$pageAuthor		= "Steffen Pingel";
+	$pageTitle 		= "Mylyn Tasks";
+
+	$Menu->setMenuItemList(array());
+	$Menu->addMenuItem("Mylyn", "/mylyn/", "_self");
+	$Menu->addMenuItem("About", "/projects/project_summary.php?projectid=mylyn.tasks", "_self");
+	$Menu->addMenuItem("Plan", "/projects/project-plan.php?projectid=mylyn.tasks", "_self");
+	
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>' . "\n\t");
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style2.css"/>' . "\n\t");
+	
+	$App->Promotion = TRUE;
+?>

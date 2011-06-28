ace8c67dc58bea9e61f8d1acf7230cdb7fd9552b
spingel <spingel>
2011-06-28 10:04:52 +0000

    NEW - bug 350531: Invalid link to 3.6 downloadable ZIP on mylyn downloads page https://bugs.eclipse.org/bugs/show_bug.cgi?id=350531

--- downloads/index.php
+++ downloads/index.php
@@ -80,7 +80,7 @@ $html = <<<EOHTML
 			<br>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/mylyn/new"><img src="/mylyn/images/star.png"/> New & Noteworthy</a> (2011-06-22). Requires <a href="http://java.sun.com/j2se/1.5.0/download.jsp">JRE 5.0</a> or later. Supports <a href="http://www.bugzilla.org/" target="_new">Bugzilla</a> 3.0 and later, <a href="http://trac.edgewall.org/" target="_new">Trac</a> 0.10 and later.
 			<ul> 
 				<li>
-					<a title="Update Site Zip" href="http://www.eclipse.org/downloads/download.php?file=/mylyn/drops/3.6/v20110608-1400/mylyn-3.6.0.v20110608-1400.zip">
+					<a title="Update Site Zip" href="http://www.eclipse.org/downloads/download.php?file=/mylyn/drops/3.6.0/v20110608-1400/mylyn-3.6.0.v20110608-1400.zip">
 						<img border=0 src="/mylyn/images/icon-save.gif">
 					</a>
 					&nbsp;&nbsp;<b>download.eclipse.org/mylyn/releases/latest</b>&nbsp;&nbsp;(Eclipse 3.5, 3.6, 3.7)

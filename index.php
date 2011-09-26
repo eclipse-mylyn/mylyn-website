3e1c11d23613e370bc23f0f88866491b93299ba9
spingel <spingel>
2011-09-26 15:23:42 +0000

    NEW - bug 194875: [website] manage eclipse.org/mylyn web site content https://bugs.eclipse.org/bugs/show_bug.cgi?id=194875

--- index.php
+++ index.php
@@ -125,12 +125,16 @@ $html = <<<EOHTML
 					<h3>News</h3>
 					<ul>
 						<li>
+							<a href="http://tasktopevents.webex.com/tasktopevents/onstage/g.php?t=a&d=660742793" target="_new">
+							<img src="images/star.png"/>&nbsp;&nbsp;Upcoming Webinar: Tasktop Sync with Mik Kersten</a> <i>September 28, 2011</i>
+						</li>
+						<li>
 		            		<a href="new/">
 		           			<img src="images/star.png"/>&nbsp;&nbsp;Mylyn 3.6.2 released</a> <i>September 23, 2011</i>
 	            		</li>
 						<li>
 		            		<a href="http://jaxenter.com/mik-kersten-jaxconf-2011-36569.html" target="_new">
-		           			<img src="images/star.png"/>&nbsp;&nbsp;Mik Kersten on Eclipse Indigo and new features in Mylyn 3.6</a> <i>June 28, 2011</i>
+		           			Mik Kersten on Eclipse Indigo and new features in Mylyn 3.6</a> <i>June 28, 2011</i>
 	            		</li>
 						<li>
 		            		<a href="new/">

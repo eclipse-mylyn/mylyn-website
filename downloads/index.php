68eae3495928cae5db2cd87a66dab6b234e372d6
Steffen Pingel <steffen.pingel@tasktop.com>
2013-11-07 20:28:36 +0100

    fix date of 3.10 release

--- downloads/index.php
+++ downloads/index.php
@@ -67,7 +67,7 @@ $html = <<<EOHTML
 						<tr> 
 			      			<td><font size=+0><a href="/mylyn/downloads/archive.php">Older Downloads</a></font></td>
 						</tr>
-						<tr><td>Download archive and support for Eclipse 3.6, 3.5, 3.4, 3.3 and 3.2</td></tr>
+						<tr><td>Download archive and support for Eclipse 3.7, 3.6, 3.5, 3.4, 3.3 and 3.2</td></tr>
 					</table>
 				</td> 
 			</tr> 
@@ -77,7 +77,7 @@ $html = <<<EOHTML
 	<!-- Release -->
 		 <div class="homeitem3col" style="margin-left:40px; width:75%">
 			<h3>3.10 Release&nbsp;<a title="More Info" href="http://wiki.eclipse.org/index.php/Mylyn/FAQ#Installation"><img border=0 src="/mylyn/images/icon-help.gif"></a></h3> 
-			<br>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/mylyn/new"><img src="/mylyn/images/star.png"/> New & Noteworthy</a> (2013-09-29). Requires <a href="http://www.oracle.com/technetwork/java/">Java SE 6</a> or later. Supports <a href="http://www.bugzilla.org/" target="_new">Bugzilla</a>, <a href="http://trac.edgewall.org/" target="_new">Trac</a>, <a href="http://hudson-ci.org/" target="_new">Hudson</a>, <a href="http://jenkins-ci.org/" target="_new">Jenkins</a> and <a href="http://code.google.com/p/gerrit/" target="_new">Gerrit</a>.
+			<br>&nbsp;&nbsp;&nbsp;<a href="http://www.eclipse.org/mylyn/new"><img src="/mylyn/images/star.png"/> New & Noteworthy</a> (2013-11-06). Requires <a href="http://www.oracle.com/technetwork/java/">Java SE 6</a> or later. Supports <a href="http://www.bugzilla.org/" target="_new">Bugzilla</a>, <a href="http://trac.edgewall.org/" target="_new">Trac</a>, <a href="http://hudson-ci.org/" target="_new">Hudson</a>, <a href="http://jenkins-ci.org/" target="_new">Jenkins</a> and <a href="http://code.google.com/p/gerrit/" target="_new">Gerrit</a>.
 			<ul> 
 				<li>
 					<a title="Update Site Zip" href="http://www.eclipse.org/downloads/download.php?file=/mylyn/drops/3.10.0/v20131025-2037/mylyn-3.10.0.v20131025-2037.zip">

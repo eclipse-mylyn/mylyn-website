248535c2307f478cbc686968b11a43062d49785b
Frank Becker <eclipse@frank-becker.de>
2014-08-30 08:37:54 +0200

    322716: [website] update bug count on contributors page
    Change-Id: I42d7fe8d6553f0ffdf192148a491385ab579cfff
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=322716
    Signed-off-by: Frank Becker <eclipse@frank-becker.de>

--- contributors/index.php
+++ contributors/index.php
@@ -18,9 +18,55 @@
 
 	# End: page-specific settings
 	#
-	  $field_seperator = ",";
+function extractCommitter($url, $committerArray)
+{
+    $committerReviews = file_get_contents($url);
+    $start=  substr($committerReviews, strpos($committerReviews, "<h3>Committers</h3>"));
+    $committerList =substr($start,19, strpos($start, "</div>"));
+    $lines = explode('</li>', strip_tags($committerList, '<li>'));
+    foreach($lines as $line) {
+      if (strlen($line)>3) {
+        $actualCommitter = substr($line, strpos($line, ">")+1);
+        if (!in_array( $actualCommitter, $committerArray)) {
+          $committerArray[] = $actualCommitter;
+        }
+      }
+    }
+    return $committerArray;
+}
+
+  $field_seperator = ",";
   $lines_seperator = "\n";
-  $content = file_get_contents("https://bugs.eclipse.org/bugs/report.cgi?bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&classification=Mylyn&f1=assigned_to&f2=assigned_to&f3=assigned_to&f4=assigned_to&f5=assigned_to&f6=assigned_to&f7=assigned_to&f8=assigned_to&o1=notequals&o2=notequals&o3=notequals&o4=notequals&o5=notequals&o6=notequals&o7=notsubstring&o8=notequals&resolution=FIXED&v1=eclipse%40Frank-Becker.de&v2=steffen.pingel%40tasktop.com&v3=david.green%40tasktop.com&v4=mik.kersten%40tasktop.com&v5=robert.elves%40tasktop.com&v6=shawn.minto%40tasktop.com&v7=mylyn-inbox&v8=r4e-triaged%40eclipse.org&x_axis_field=assigned_to_realname&width=1024&height=600&action=wrap&ctype=csv&format=table");
+  $committerArray =extractCommitter("https://projects.eclipse.org/projects/mylyn/who", array());
+  $committerArray =extractCommitter("https://projects.eclipse.org/projects/mylyn.builds/who", $committerArray);
+  $committerArray =extractCommitter("https://projects.eclipse.org/projects/mylyn.commons/who", $committerArray);
+  $committerArray =extractCommitter("https://projects.eclipse.org/projects/mylyn.context/who", $committerArray);
+  $committerArray =extractCommitter("https://projects.eclipse.org/projects/mylyn.docs/who", $committerArray);
+  $committerArray =extractCommitter("https://projects.eclipse.org/projects/mylyn.incubator/who", $committerArray);
+  $committerArray =extractCommitter("https://projects.eclipse.org/projects/mylyn.reviews/who", $committerArray);
+  $committerArray =extractCommitter("https://projects.eclipse.org/projects/mylyn.tasks/who", $committerArray);
+  $committerArray =extractCommitter("https://projects.eclipse.org/projects/mylyn.versions/who", $committerArray);
+
+  $committerList = "";
+  $committerNotList = "";
+  $committerIndex = 0;
+  foreach($committerArray as $committerName) {
+    if (strlen($committerName)>0) {
+       $committerIndex++;
+       $committerList = $committerList . '&f'. $committerIndex
+       				  . '=assigned_to&o'. $committerIndex
+                      . '=allwordssubstr&v'. $committerIndex
+       				  . '=' . str_replace(' ', '+', $committerName);
+       $committerNotList = $committerNotList . '&n'. $committerIndex . '=1';
+    }
+  }
+
+  $committerpart = "bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&classification=Mylyn&resolution=FIXED"
+					. "&j_top=OR" . $committerList;
+  $url= "https://bugs.eclipse.org/bugs/report.cgi?"
+  								. $committerpart
+  								. "&x_axis_field=assigned_to_realname&width=1024&height=600&action=wrap&ctype=csv&format=table";
+  $content = file_get_contents($url);
   $lines = explode($lines_seperator, $content);
   $sort_ord = array();
   $sort_rec = array();
@@ -29,18 +75,52 @@
     if (is_array($fields) == true) {
       if ($fields[1]> 1) {
         $sort_ord[] = $fields[1];
-        $sort_rec[] = $fields; 
+        $sort_rec[] = $fields;
       }
     }
   }
-  $htmlDynamic = '<br><br>  sorted list (count descending)<br>';
+  $htmlDynamic = '<h1>Committer</h1>sorted list (count descending)<br>';
   $htmlDynamic = $htmlDynamic . '<table id="user_list_sort" border="1"><tbody><tr><td>Name</td><td>Count</td></tr>';
   array_multisort($sort_ord, SORT_NUMERIC, SORT_DESC, $sort_rec);
   foreach($sort_rec as $sort_recfields) {
-    $htmlDynamic = $htmlDynamic . '<tr><td>'.str_replace('"','',$sort_recfields[0]).' </td><td><a href="https://bugs.eclipse.org/bugs/buglist.cgi?action=wrap&amp;bug_status=RESOLVED&amp;bug_status=VERIFIED&amp;bug_status=CLOSED&amp;classification=Mylyn&amp;f1=assigned_to&amp;f2=assigned_to&amp;f3=assigned_to&amp;f4=assigned_to&amp;f5=assigned_to&amp;f6=assigned_to&amp;f7=assigned_to&amp;f8=assigned_to&amp;o1=notequals&amp;o2=notequals&amp;o3=notequals&amp;o4=notequals&amp;o5=notequals&amp;o6=notequals&amp;o7=notsubstring&amp;o8=notequals&amp;resolution=FIXED&amp;v1=eclipse%40Frank-Becker.de&amp;v2=steffen.pingel%40tasktop.com&amp;v3=david.green%40tasktop.com&amp;v4=mik.kersten%40tasktop.com&amp;v5=robert.elves%40tasktop.com&amp;v6=shawn.minto%40tasktop.com&amp;v7=mylyn-inbox&amp;v8=r4e-triaged%40eclipse.org&amp;assigned_to_realname='.urlencode(str_replace('"','',$sort_recfields[0])).'">'. $sort_recfields[1].'</a></td></tr>';
+    $htmlDynamic = $htmlDynamic . '<tr><td>' .str_replace('"','',$sort_recfields[0])
+    		.' </td><td><a href="https://bugs.eclipse.org/bugs/buglist.cgi?action=wrap&amp;'.str_replace('&','&amp;',$committerpart)
+    		."&amp;assigned_to_realname=".urlencode(str_replace('"','',$sort_recfields[0])).'">'
+    		. $sort_recfields[1].'</a></td></tr>';
+  }
+
+  $htmlDynamic = $htmlDynamic . '</table><br><br><br>';
+    $url= "https://bugs.eclipse.org/bugs/report.cgi?"
+  								. str_replace('&j_top=OR','&j_top=AND',$committerpart)
+    							. $committerNotList
+  								. "&x_axis_field=assigned_to_realname&width=1024&height=600&action=wrap&ctype=csv&format=table";
+
+  $content = file_get_contents($url);
+  $lines = explode($lines_seperator, $content);
+  $sort_ord = array();
+  $sort_rec = array();
+  foreach($lines as $line) {
+    $fields = explode($field_seperator, $line);
+    if (is_array($fields) == true) {
+      if ($fields[1]> 1) {
+        $sort_ord[] = $fields[1];
+        $sort_rec[] = $fields;
+      }
+    }
+  }
+  $htmlDynamic = $htmlDynamic . '<h1>Contributors</h1>sorted list (count descending)<br>';
+  $htmlDynamic = $htmlDynamic . '<table id="user_list_sort" border="1"><tbody><tr><td>Name</td><td>Count</td></tr>';
+  array_multisort($sort_ord, SORT_NUMERIC, SORT_DESC, $sort_rec);
+  foreach($sort_rec as $sort_recfields) {
+    $htmlDynamic = $htmlDynamic . '<tr><td>' .str_replace('"','',$sort_recfields[0])
+    		.' </td><td><a href="https://bugs.eclipse.org/bugs/buglist.cgi?action=wrap&amp;'
+    		.str_replace('&','&amp;',str_replace('&j_top=OR','&j_top=AND',$committerpart))
+    		. $committerNotList
+    		."&amp;assigned_to_realname=".urlencode(str_replace('"','',$sort_recfields[0])).'">'
+    		. $sort_recfields[1].'</a></td></tr>';
   }
   $htmlDynamic = $htmlDynamic . '</table>';
-		
+
 	# Paste your HTML content between the EOHTML markers!	
 	$htmlStart = <<<EOHTML
 <div id="maincontent">
@@ -59,7 +139,6 @@ EOHTML;
 		$side_announce
 	</div>
 </div>
-
 EOHTML;
 
 $html= $htmlStart . $htmlDynamic .$htmlEnd;

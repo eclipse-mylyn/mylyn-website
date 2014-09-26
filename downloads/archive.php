f37b4fe24549c80ce45d0c01850f83a79784071b
Sam Davis <sam.davis@tasktop.com>
2014-09-26 11:05:00 -0700

    439453: [release] release Mylyn 3.13 for Luna SR1
    Change-Id: I30fc033d43ce4ed1ecfb559c14177e8eedf0d124
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=439453

--- downloads/archive.php
+++ downloads/archive.php
@@ -1164,6 +1164,12 @@
 			<a name="baseline"></a><h3>API Baselines (for contributors)</h3> 
 			<ul>
 				<li> 
+					<a title="Mylyn 3.13.0" href="https://www.eclipse.org/downloads/download.php?file=/mylyn/drops/3.13.0/mylyn-3.13.0-api.zip">
+						<img border=0 src="/mylyn/images/icon-save.gif">
+					</a>
+					&nbsp;&nbsp;<b>Mylyn 3.13.0 API Baseline</b>
+				</li>
+				<li> 
 					<a title="Mylyn 3.12.0" href="https://www.eclipse.org/downloads/download.php?file=/mylyn/drops/3.12.0/mylyn-3.12.0-api.zip">
 						<img border=0 src="/mylyn/images/icon-save.gif">
 					</a>
